be6b3248b097ff8746be3de1504c9c173e91bb89
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-06-14 13:06:36 +0530

    Time: 5 ms (42.16%), Space: 44.5 MB (89.89%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,97 +1,96 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-    List<List<String>> allBoards=new ArrayList<>();
-    char[][] boards=new char[n][n];
-    helper(boards,allBoards,0);
-    
-    return allBoards;
-    }
-    public void helper(char[][] boards,List<List<String>> allBoards,int col){
+        List<List<String>> allBoards=new ArrayList<>();
+        char[][] boards=new char[n][n];
 
+        helper(boards,allBoards,0);
+        return allBoards;
+    }
+
+    public void helper(char[][] board,List<List<String>> allBoards,int col){
         //BASE CASE
-        if(col==boards.length){
-            saveBoard(boards,allBoards);
-            return;
-        }
-        for(int row=0;row<boards.length;row++){
-            if(isSafe(row,col,boards)){
-                boards[row][col]='Q';
-                helper(boards,allBoards,col+1);
-                boards[row][col]='.';
+        if(col==board.length){
+            saveBoard(board,allBoards);
+                return;
+            }
+
+        for(int row=0;row<board.length;row++){
+            if(isSafe(row,col,board)){
+                //adding 'Q' IF true
+                board[row][col]='Q';
+                helper(board, allBoards,col+1);
+                //removing 'Q' in the next position
+                board[row][col]='.';
             }
         }
     }
-    public boolean isSafe(int row,int col,char[][] boards){
 
-        //HORIZONTAL LINE
-        for(int j=0;j<boards.length;j++){
-            if(boards[row][j]=='Q'){
+    public void saveBoard(char[][] board,List<List<String>> allBoards){
+        String row="";
+        List<String> newBoard=new ArrayList<>();
+
+        for(int i=0;i<board.length;i++){
+            row="";
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='Q'){
+                    row +='Q';
+                }else{
+                    row +='.';
+                }
+            }
+            newBoard.add(row);
+        }
+        allBoards.add(newBoard);
+    }
+    public boolean isSafe(int row,int col,char[][] board){
+
+        //horizontal
+        for(int j=0;j<board.length;j++){
+            if(board[row][j]=='Q'){
                 return false;
             }
         }
 
-        //VERTICAL LINE
-        for(int i=0;i<boards[0].length;i++){
-            if(boards[i][col]=='Q'){
+        //vertical
+        for(int i=0;i<board.length;i++){
+            if(board[i][col]=='Q'){
                 return false;
             }
         }
 
         //upper left
         int r=row;
-        for(int c=col;c>=0 && r>=0;c--,r--){
-            if(boards[r][c]=='Q'){
+        for(int c=col;c>=0&&r>=0;c--,r--){
+            if(board[r][c]=='Q'){
                 return false;
             }
         }
 
         //upper right
         r=row;
-        for(int c=col;c<boards.length && r>=0;c++,r--){
-            if(boards[r][c]=='Q'){
+        for(int c=col;c<board.length && r>=0;c++,r--){
+           if(board[r][c]=='Q'){
                 return false;
-            }
+            } 
         }
 
         //lower left
         r=row;
-        for(int c=col;c>=0 && r<boards.length;c--,r++){
-            if(boards[r][c]=='Q'){
+        for(int c=col;c>=0&&r<board.length;c--,r++){
+            if(board[r][c]=='Q'){
                 return false;
-            }
+            } 
         }
 
         //lower right
         r=row;
-        for(int c=col;c<boards.length && r<boards.length;c++,r++){
-             if(boards[r][c]=='Q'){
+        for(int c=col;c<board.length && r<board.length;c++,r++){
+            if(board[r][c]=='Q'){
                 return false;
-            }
+            } 
         }
 
         return true;
     }
-
-       public static void saveBoard(char[][] boards,List<List<String>> allBoards){
-        String row="";
-
-        List<String> newBoard=new ArrayList<>();
-        for(int i=0;i<boards.length;i++){
-             row="";
-                for(int j=0;j<boards[0].length;j++){
-                    if(boards[i][j]=='Q'){
-                        row +='Q';
-                    }else{
-                        row +='.';
-                    }
-                 
-                }
-                   newBoard.add(row);
-       
-                
-        }
-         allBoards.add(newBoard);
-       }
-
     
 }
