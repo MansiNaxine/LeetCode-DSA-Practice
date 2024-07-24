3c81ffe7631fea4c91442ac3a44e8bcebbe46597
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-07-24 18:34:13 +0530

    Time: 1 ms (86.22%), Space: 40.5 MB (74.55%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        //If we know the given variable is having value 0 and 1 then square root of these numbers are same
+        if(x==0 || x==1){
+            return x;
+        }
+
+        int start=1;
+        int end=x;
+
+        //we are iterating till the number and checking if square of mid is equal to or gretaer or smaller than given number
+        while(start<=end){
+            int  mid=start+(end-start)/2;
+
+            if(x==mid*mid){
+                return mid;
+            }else if((long)x<(long)mid*mid){//if square of mid is less than the given number then obviously our end will be the answer
+                    end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+
+        return Math.round(end) ;
+        
+    }
+}
