7ec08a12f66adbf77ca7e3b1a9d7a6933a0fd1a2
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-09-06 18:50:42 +0530

    Time: 16 ms (6.03%), Space: 41.4 MB (18.88%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,67 +1,91 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        helper(board,0,0);
+
+        if(solve(board)){
+            display(board);
+        }
+        System.out.println();
+        
     }
 
-    public boolean helper(char[][] board,int row,int col){
+    public static void display(char[][] board){
+        for(char[] arr:board){
+            for(char element:arr){
+                System.out.print(element+" ");
+            }
+            System.out.println();
+        }
+    }
 
-        if(row==board.length){
+
+    public static boolean solve(char[][] board){
+        int row=-1;
+        int col=-1;
+
+        boolean isEmpty=true;
+
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='.'){
+                    row=i;
+                    col=j;
+                    isEmpty=false;
+                    break;
+                }
+            }
+
+            if(!isEmpty){
+                break;
+            }
+        }
+
+        if(isEmpty){
             return true;
         }
 
-        int nrow=0;
-        int ncol=0;
+        for(int num=1; num<=9;num++){
+            if(isSafe(board,row,col,num)){
+                board[row][col]=(char)(num+'0');
 
-        if(col!=board.length-1){
-            nrow=row;
-            ncol=col+1;
-        }else{
-            nrow=row+1;
-            ncol=0;
-        }
-
-        if(board[row][col]!='.'){
-            if(helper(board,nrow,ncol)){
-                return true;
-            }
-        }else{
-                for(int i=1;i<=9;i++){
-                    if(isSafe(board,row,col,i)){
-                        board[row][col]=(char)(i+'0');
-                        if(helper(board,nrow,ncol)){
-                            return true;
-                        }else{
-                          board[row][col]='.';
-                        }
-                    }
-
+                if(solve(board)){
+                    return true;
+                }else{
+                    board[row][col]='.';
                 }
-              
             }
-        
-      return false;
-    }
-    public boolean isSafe(char[][] board,int row,int col,int number){
+        }
 
-        //row and column
+        return false;
+
+    }
+
+
+    public static boolean isSafe(char[][] board,int row,int col, int num){
+
+        //to check in rows
         for(int i=0;i<board.length;i++){
-            if(board[i][col]==(char)(number+'0')){
-                return false;
-            }
-            if(board[row][i]==(char)(number+'0')){
+            if(board[row][i]==(char)(num+'0')){
                 return false;
             }
         }
 
-        //GRID
-        int sr=(row/3)*3;
-        int sc=(col/3)*3;
+        //to check in columns
+        for(int i=0;i<board.length;i++){
+            if(board[i][col]==(char)(num+'0')){
+                return false;
+            }
+        }
 
-        for(int i=sr;i<sr+3;i++){
-            for(int j=sc;j<sc+3;j++){
-                if(board[i][j]==(char)(number+'0')){
+        //to check in 3*3 matrix
+        int sqrt=(int)(Math.sqrt(board.length));
+        int rowStart=row-row%3;
+        int colStart=col-col%3;
+
+        for(int i=rowStart;i<rowStart+sqrt;i++){
+            for(int j=colStart;j<colStart+sqrt;j++){
+                if(board[i][j]==(char)(num+'0')){
                     return false;
-                }     
+                }
             }
         }
 
