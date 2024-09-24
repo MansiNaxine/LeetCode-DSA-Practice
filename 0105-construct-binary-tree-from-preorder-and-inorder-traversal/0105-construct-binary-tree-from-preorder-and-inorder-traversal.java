5332e0b3539e451683ea67840a7669a60f4de7da
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-09-24 15:37:39 +0530

    Time: 2 ms (68.46%), Space: 44.2 MB (67.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,25 +17,56 @@ class Solution {
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
 
-        if(preorder.length == 0){
-            return null;
-        }
+        // if(preorder.length == 0){
+        //     return null;
+        // }
 
-        int root=preorder[0];
-        int index=0;
+        // int root=preorder[0];
+        // int index=0;
+
+        // for(int i=0;i<inorder.length;i++){
+        //     if(inorder[i]==root){
+        //         index=i;
+        //     }
+        // }
+
+        // TreeNode node=new TreeNode(root);
+
+        // node.left=buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index));
+        // node.right=buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
+
+        // return node;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
 
         for(int i=0;i<inorder.length;i++){
-            if(inorder[i]==root){
-                index=i;
-            }
+            map.put(inorder[i],i);
         }
+        int[] index={0};
+        return helper(preorder,inorder,map,0,preorder.length-1,index);
+ 
+    }
 
-        TreeNode node=new TreeNode(root);
 
-        node.left=buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index));
-        node.right=buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
+    public TreeNode helper(int[] preOrder, int[] inOrder,HashMap<Integer,Integer> map,int left,int right,int[] index ){
+
+        if(left>right){
+            return null;
+        }
+        int current=preOrder[index[0]];
+        index[0]++;
+
+        TreeNode node=new TreeNode(current);
+        
+
+        //leaf node condition
+        if(left==right){
+            return node;
+        }
+        int CurrentIndex=map.get(current);
+        node.left=helper(preOrder,inOrder,map,left,CurrentIndex-1,index);
+        node.right=helper(preOrder,inOrder,map,CurrentIndex+1,right,index);
 
         return node;
- 
     }
 }
