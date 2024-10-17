67016d5219b0678cd5fc0831878e241cb7599a5b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-17 23:35:30 +0530

    Time: 1 ms (99.42%), Space: 57.4 MB (8.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -6,9 +6,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum +=nums[i];
             
-            if(max<sum){
-                max=sum;
-            }
+            max=Math.max(max,sum);
 
             if(sum<0){
                 sum=0;
