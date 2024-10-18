873daceffa2edb4faa926c1c39cc7c3165b3dc1b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-18 22:26:06 +0530

    Time: 0 ms (100%), Space: 57.6 MB (14.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,20 +2,20 @@ class Solution {
     public void rotate(int[] nums, int k) {
 
         int arr[]=new int[k];
-        int j=0;
+        //int j=0;
         int n=nums.length;
         k=k%n;
 
         //place elements in new array from kth index;
-        for(int i=n-k;i<n;i++){
-            arr[j]=nums[i];
-            j++;
-        }
-        
+        // for(int i=n-k;i<n;i++){
+        //     arr[j]=nums[i];
+        //     j++;
+        // }
+
         //move elements from original array to the remaining index of nums and also from newly created array into original array;
-        for(int i=n-1;i>=0;i--){
-            nums[i]=i<k?arr[i]:nums[i-k];
-        }  
+        // for(int i=n-1;i>=0;i--){
+        //     nums[i]=i<k?arr[i]:nums[i-k];
+        // }  
 
         //Second Approach
         //1.Reverse the array with constant space
@@ -38,5 +38,24 @@ class Solution {
         //     nums[s]=nums[i];
         //     nums[i]=temp;
         // }
+
+        //3. next approach to optimize time complexity
+        int[] reverseArray1=reverseArray(nums,n-1,0);
+        int[] reverseArray2=reverseArray(nums,k-1,0);
+        int[] reverseArray3=reverseArray(nums,n-1,k);
+
+    }
+
+    public static int[] reverseArray(int[] nums,int i,int j){
+
+        while(j<i){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i--;
+            j++;
+        }
+
+        return nums;
     }
 }
