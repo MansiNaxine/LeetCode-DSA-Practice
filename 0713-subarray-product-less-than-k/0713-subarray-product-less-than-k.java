37d9d44e82760842cea8d78c80fdb191be651c7d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-18 16:19:31 +0530

    Time: 863 ms (7.83%), Space: 47.7 MB (50.73%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -4,9 +4,7 @@ class Solution {
         
         int count=0;
         for(int i=0;i<nums.length;i++){
-
             int mul=1;
-
             for(int j=i;j<nums.length;j++){
 
                 mul *=nums[j];
@@ -14,9 +12,8 @@ class Solution {
                     if(mul>=k){
                         break;
                     }
-                    
-                count++;
-                
+
+                count++;  
             }
         }
         return count;
