d570bbae616c00637f3b5d504d964ce3a3456576
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-20 18:46:28 +0530

    Time: 1 ms (89.95%), Space: 41.1 MB (27.65%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -3,13 +3,13 @@ class Solution {
 
         int count=0;
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
+             for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j] && i<j){
                     count++;
                 }
             }
         }
-
+       
         return count;
         
     }
