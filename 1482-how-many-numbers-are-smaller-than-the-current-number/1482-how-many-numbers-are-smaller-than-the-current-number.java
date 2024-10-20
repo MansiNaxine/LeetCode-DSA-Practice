6ad181cba6ac9b2b5df50ac137fcd8e29cb05218
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-20 19:38:33 +0530

    Time: 10 ms (22.13%), Space: 44.3 MB (58.87%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -4,7 +4,7 @@ class Solution {
         int ans[]=new int[nums.length];
 
         for(int i=0;i<nums.length;i++){
-            int count=0;
+            int count = 0;
             for(int j=0;j<nums.length;j++){
                 if(nums[j]<nums[i] && i!=j){
                     count++;
