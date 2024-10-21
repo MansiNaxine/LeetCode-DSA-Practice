64dff869fb17e9bd1f66822d268b3e29ba0962c4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-21 20:23:39 +0530

    Time: 0 ms (100%), Space: 42 MB (11.27%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
@@ -3,30 +3,42 @@ class Solution {
         int n=nums.length;
         int target[]=new int[n];
 
-        for(int i=0;i<nums.length;i++){
-            target[i]=-1;
-        }
+        //more time complexity O(n+n*n);
 
-        int i=0;
-        while(i<nums.length){
-            int k=n-1;
-            int j=index[i];
-            if(target[index[i]]==-1){
-                target[index[i]]=nums[i];
-            }else{
-                if(j<i){
-                    while(k>j){
-                        target[k]=target[k-1];
-                        k--;
-                    }
-                }
-                target[index[i]]=nums[i];
-            }
+        // for(int i=0;i<nums.length;i++){
+        //     target[i]=-1;
+        // }
+
+        // int i=0;
+        // while(i<nums.length){
+        //     int k=n-1;
+        //     int j=index[i];
+        //     if(target[index[i]]==-1){
+        //         target[index[i]]=nums[i];
+        //     }else{
+        //         if(j<i){
+        //             while(k>j){
+        //                 target[k]=target[k-1];
+        //                 k--;
+        //             }
+        //         }
+        //         target[index[i]]=nums[i];
+        //     }
                 
 
-            i++;
+        //     i++;
+        // }
+
+        
+        //with optimized solution
+        List<Integer> list=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            list.add(index[i],nums[i]);
         }
 
+        for(int i=0;i<n;i++){
+            target[i]=list.get(i);
+        }
 
         return target;
         
