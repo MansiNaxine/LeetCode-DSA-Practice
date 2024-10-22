e35d0e703df3e3ae3fccde49022f292dc4b0d3bc
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-22 18:20:58 +0530

    Time: 1 ms (99.43%), Space: 56.8 MB (80.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -6,10 +6,8 @@ class Solution {
         int sum=0;
         int max=nums[0];
         for(int i=0;i<nums.length;i++){
-
             sum +=nums[i];
             max=Math.max(max,sum);
-
             if(sum<0){
                 sum=0;
             }
