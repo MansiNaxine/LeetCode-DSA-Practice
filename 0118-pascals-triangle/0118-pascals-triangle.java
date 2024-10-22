74b4c5e1af3f38d9b4ea36bd798711610a62090c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-22 20:14:13 +0530

    Time: 1 ms (84.62%), Space: 42.1 MB (27.6%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -7,12 +7,11 @@ class Solution {
 
         answer.add(new ArrayList<>(firstRow));
         int middle=0;
-
         for(int i=1;i<numRows;i++){
             List<Integer> currentRow=new ArrayList<>();
             currentRow.add(1);
 
-            //Middle Element Logic
+            //Logic for middle element
             for(int j=1;j<=middle;j++){
                 int element=answer.get(i-1).get(j)+answer.get(i-1).get(j-1);
                 currentRow.add(element);
