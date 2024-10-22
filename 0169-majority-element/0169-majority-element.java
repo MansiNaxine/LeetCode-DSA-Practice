93eeb4fe1f5dd6ca7d4ab2f3b369ca2ed70bd4c9
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-22 19:28:14 +0530

    Time: 1 ms (99.81%), Space: 53 MB (40.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,24 +5,38 @@ class Solution {
         int n=nums.length/2;
 
         int ans=0;
+        int count=0;
 
-        Map<Integer,Integer> map=new HashMap<>();
+        // Map<Integer,Integer> map=new HashMap<>();
 
-        for(int num:nums){
+        // for(int num:nums){
 
-            if(!map.containsKey(num)){
-                map.put(num,1);
+        //     if(!map.containsKey(num)){
+        //         map.put(num,1);
+        //     }else{
+        //         map.put(num,map.get(num)+1);
+        //     }
+        // }
+
+        // for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        //     if(entry.getValue()>n){
+        //         ans=entry.getKey();
+        //         break;
+        //     }
+        // }
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                ans=nums[i];
+                count=1;
             }else{
-                map.put(num,map.get(num)+1);
+                if(ans==nums[i]){
+                    count++;
+                }else{  
+                        count--;
+                    }
+                 }
             }
-        }
-
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>n){
-                ans=entry.getKey();
-                break;
-            }
-        }
 
         return ans;
         
