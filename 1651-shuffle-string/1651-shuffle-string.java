3b86df541db97f7c3f2c57647dff2d39be8b4fc5
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-25 11:38:57 +0530

    Time: 1 ms (73.55%), Space: 44.6 MB (45.78%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+
+        char[] arr=new char[indices.length];
+
+        for(int i=0;i<s.length();i++){
+            arr[indices[i]]=s.charAt(i);
+        }
+
+        return String.valueOf(arr);
+        
+    }
+}
