52d5211b39cbe22f83a99ba101a0a020dafd0acd
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-26 15:02:13 +0530

    Time: 8 ms (67.61%), Space: 49.2 MB (60.88%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,18 +1,28 @@
 class NumArray {
 
-    int[] preSum;
+    int prefix[];
 
     public NumArray(int[] nums) {
-        this.preSum=nums;
-        preSum[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-             preSum[i]=nums[i]+preSum[i-1];
+        this.prefix=nums;
+        int sum=0;
+        prefix[0]=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum +=nums[i];
+            prefix[i]=sum;
         }
+        
     }
     
     public int sumRange(int left, int right) {
-        
-        return left==0?preSum[right]:preSum[right]-preSum[left-1];
+        int sum=0;
+
+        if(left==0){
+            sum=prefix[right];
+        }else{
+            sum=prefix[right]-prefix[left-1];
+        }
+         
+        return sum;
     }
 }
 
