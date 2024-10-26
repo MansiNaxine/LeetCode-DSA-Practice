86a706f3aa5707313fd6471a3e8fa18c890d12e9
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-26 14:05:03 +0530

    Time: 34 ms (37.51%), Space: 51.3 MB (64.66%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
+        //1:- Sort the array
+        //2:- then iterate the array and calculate the sum=-nums[i]
+        //3:- Check for the nums[j]+nums[k]==sum, then return i,j,k else j++,k--;
+        //4:- Check not to include duplicates like nums[i],nums[j],nums[k], CALCULATE DISTINCT PAIRS
+
         Arrays.sort(nums);
-
         List<List<Integer>> list=new ArrayList<>();
-        
-        int n=nums.length;
-
         int i=0;
-
+        int n=nums.length;
         while(i<n){
             int target=-nums[i];
-            int fixedI=nums[i];
             int j=i+1;
             int k=n-1;
+            int duplicateI=nums[i];
+
             while(j<k){
                 if(nums[j]+nums[k]==target){
-                    List<Integer> currentArray=new ArrayList<>();
-                    currentArray.add(nums[i]);
-                    currentArray.add(nums[j]);
-                    currentArray.add(nums[k]);
+                    List<Integer> currentArr=new ArrayList<>();
+                    currentArr.add(nums[i]);
+                    currentArr.add(nums[j]);
+                    currentArr.add(nums[k]);
 
-                    list.add(new ArrayList<Integer>(currentArray));
+                    list.add(new ArrayList<>(currentArr));
 
-                    while(j<k && nums[j]==currentArray.get(1)){
+                    //Check for duplicates jth and kth elements
+                    while(j<k && nums[j]==currentArr.get(1)){
                         j++;
                     }
 
-                    while(k>j && nums[k]==currentArray.get(2)){
+                    while(k>j && nums[k]==currentArr.get(2)){
                         k--;
                     }
-
                 }else if(nums[j]+nums[k]<target){
                     j++;
                 }else{
@@ -38,11 +40,13 @@ class Solution {
                 }
             }
 
-            while(i<n && fixedI==nums[i]){
+            //Check for ith duplicate element
+            while(i<n && duplicateI==nums[i]){
                 i++;
             }
 
         }
+
         return list;
     }
 }
