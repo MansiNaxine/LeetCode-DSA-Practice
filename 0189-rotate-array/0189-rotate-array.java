03daa7307ab04b05ac9b1c771ae8e926a55c8e9f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-26 20:41:04 +0530

    Time: 1 ms (49.81%), Space: 57.7 MB (14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,61 +1,24 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        
+        k=k%nums.length;
 
-        int arr[]=new int[k];
-        //int j=0;
-        int n=nums.length;
-        k=k%n;
+        int rotateArray[]=new int[k];
 
-        //place elements in new array from kth index;
-        // for(int i=n-k;i<n;i++){
-        //     arr[j]=nums[i];
-        //     j++;
-        // }
-
-        //move elements from original array to the remaining index of nums and also from newly created array into original array;
-        // for(int i=n-1;i>=0;i--){
-        //     nums[i]=i<k?arr[i]:nums[i-k];
-        // }  
-
-        //Second Approach
-        //1.Reverse the array with constant space
-        // for(int i=n-1,j=0;j<i;i--,j++){
-        //     int temp=nums[i];
-        //     nums[i]=nums[j];
-        //     nums[j]=temp;
-        // }
-
-        // //2.Reverse till K and then reverse Remaining elements
-        // for(int i=k-1,d=0;d<i;i--,d++){
-        //     int temp=nums[d];
-        //     nums[d]=nums[i];
-        //     nums[i]=temp;
-        // }
-
-        // //3.Reverese the other remaining elements
-        // for(int i=n-1,s=k;s<i;i--,s++){
-        //     int temp=nums[s];
-        //     nums[s]=nums[i];
-        //     nums[i]=temp;
-        // }
-
-        //3. next approach to optimize time complexity
-        int[] reverseArray1=reverseArray(nums,n-1,0);
-        int[] reverseArray2=reverseArray(nums,k-1,0);
-        int[] reverseArray3=reverseArray(nums,n-1,k);
-
-    }
-
-    public static int[] reverseArray(int[] nums,int i,int j){
-
-        while(j<i){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i--;
-            j++;
+        int r=0;
+        //first place the elements fron kth index to rotateArray array
+        for(int i=nums.length-k;i<nums.length;i++){
+            rotateArray[r]=nums[i];
+            r++;
+        }
+        //shift all the elements in nums array from 0th index to last i.e. after kth index
+        for(int i=nums.length-1;i>=k;i--){
+            nums[i]=nums[i-k];
         }
 
-        return nums;
+        //copy elements from rotateArray
+        for(int i=0;i<k;i++){
+            nums[i]=rotateArray[i];
+        }
     }
 }
