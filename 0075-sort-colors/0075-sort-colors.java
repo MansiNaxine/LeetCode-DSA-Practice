171bd6f0e993af7ff3c0eba315906dbc245bfedb
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-26 19:53:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -8,27 +8,23 @@ class Solution {
             arr[nums[i]]++;
         }
 
-        int z=arr[0];
-        int o=arr[1];
-        int t=arr[2];
+        // int z=arr[0];
+        // int o=arr[1];
+        // int t=arr[2];
         int j=0;
 
          while(j<nums.length){
-            while(z>0){
+            while(j<arr[0]){
                 nums[j]=0;
-
-                z--;
                 j++;
             }
 
-             while(o>0){
+             while(j<arr[0]+arr[1]){
                 nums[j]=1;
-                o--;
                 j++;
             }
-             while(t>0){
+             while(j<arr[0]+arr[1]+arr[2]){
                 nums[j]=2;
-                t--;
                 j++;
             }
       }
