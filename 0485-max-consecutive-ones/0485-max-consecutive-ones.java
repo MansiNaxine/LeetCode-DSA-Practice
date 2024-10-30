32040b979877bcce54cb66f56a6011735662ac9d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-10-30 18:47:29 +0530

    Time: 2 ms (94.48%), Space: 50.1 MB (48.74%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -4,16 +4,15 @@ class Solution {
         int maxCount=0;
         int count=0;
         for(int num:nums){
-            if(num==0){
-                count=0;
-            }else{
+            if(num==1){
                 count++;
+            }else{
                 maxCount=Math.max(maxCount,count);
-            }
-            
+                count=0;
+            } 
         }
 
-        return maxCount;
+        return Math.max(maxCount,count);
         
     }
 }
