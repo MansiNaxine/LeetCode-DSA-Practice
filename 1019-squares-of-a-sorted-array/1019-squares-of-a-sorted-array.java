d825b59b1df2172e90598be395952f0b0728f76f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-11-06 18:28:40 +0530

    Time: 1 ms (100%), Space: 47.3 MB (35.69%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -3,23 +3,45 @@ class Solution {
 
         int n=nums.length;
 
-        for(int i=0;i<n;i++){
-            nums[i] *= nums[i];
-        }
+        //1.BruteForce Approach
 
-        for(int i=0;i<n-1;i++){
+        // for(int i=0;i<n;i++){
+        //     nums[i] *= nums[i];
+        // }
 
-            for(int j=0;j<n-1-i;j++){
+        // for(int i=0;i<n-1;i++){
 
-                if(nums[j]>=nums[j+1]){
+        //     for(int j=0;j<n-1-i;j++){
 
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
+        //         if(nums[j]>=nums[j+1]){
+
+        //             int temp=nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //         }
+        //     }
+        // }
+
+        //2.Optimized Approach
+        int answer[]=new int[n];
+        int index=n-1;
+        int i=0;
+        int j=n-1;
+
+        while(i<=j){
+
+            if(Math.abs(nums[i])>=Math.abs(nums[j])){
+                answer[index]=nums[i]*nums[i];
+                i++;
+            }else{
+                answer[index]=nums[j]*nums[j];
+                j--;
             }
+
+            index--;
+
         }
         
-        return nums;
+        return answer;
     }
 }
