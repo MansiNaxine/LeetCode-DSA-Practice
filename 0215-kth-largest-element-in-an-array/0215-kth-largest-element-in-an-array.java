0e3dcb4e91de757697e0b8dd3f344dec9ac8b7c6
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-11-24 23:38:53 +0530

    Time: 2201 ms (5.01%), Space: 62.1 MB (5.17%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+        int n = nums.length;
+        int index = n - k;
+
+        return quickSort(nums, 0, n-1, index);
+    }
+
+    public int quickSort(int[] nums, int start, int end, int index){
+
+        if(start == end){
+            return nums[start];
+        }
+
+        int k = partition(nums, start, end);
+
+        if(k == index){
+
+            return nums[index];
+
+        }else if( k < index){
+
+            return quickSort(nums, k+1, end, index);
+
+        }else{
+
+            return quickSort(nums, start, k-1, index);
+        }
+    }
+
+    public int partition(int[] nums, int start, int end){
+
+        int pivot = nums[end];
+
+        int i = start;
+
+        for(int j=start; j<=end-1; j++){
+
+            if(nums[j] < pivot){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                i++;
+            }
+        }
+
+        int temp = nums[i];
+        nums[i] = nums[end];
+        nums[end] = temp;
+
+        return i;
+    }
+}
