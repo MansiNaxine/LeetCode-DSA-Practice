022f252346cfee8056adc1ee62268be0120efca7
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-11-27 18:11:25 +0530

    Time: 2 ms (50.41%), Space: 44.6 MB (7.18%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    List<String> answer = new ArrayList<>();
+    ArrayList<Character> currSeq = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+
+        recursion(0, 0, 2*n);
+        return answer;
+    }
+
+    public void recursion(int index, int sum, int length){
+
+        //base case
+        if(index == length){
+
+            if(sum == 0){
+
+                StringBuilder sb= new StringBuilder();
+                for(int i=0; i< currSeq.size(); i++){
+                    sb.append(currSeq.get(i));
+                }
+                answer.add(sb.toString());
+            }
+                return;
+            
+            
+        }
+        //recursive case
+        //first for open paranthesis
+        if(sum < length/2){
+            currSeq.add('(');
+            recursion(index+1, sum+1, length);
+            currSeq.remove(currSeq.size()-1);
+        }
+
+        //Second for close paranthesis
+        if(sum > 0){
+            currSeq.add(')');
+            recursion(index+1, sum-1, length);
+            currSeq.remove(currSeq.size()-1);
+         }
+        
+    }
+}
