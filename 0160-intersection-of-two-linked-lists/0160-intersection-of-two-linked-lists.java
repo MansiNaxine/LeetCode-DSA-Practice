f8e363e6a1030be556d04774476bdd413051a96a
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2024-12-26 16:45:45 +0530

    Time: 1 ms (99.9%), Space: 48.8 MB (30.76%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,45 +11,54 @@
  */
 public class Solution {
 
-    public int getLength(ListNode head) {
+    // public int getLength(ListNode head) {
         
-        int len = 0;
-        ListNode temp = head;
-        while(temp != null) {
-            temp = temp.next;
-            len++;
-        }
-        return len;
-    }
+    //     int len = 0;
+    //     ListNode temp = head;
+    //     while(temp != null) {
+    //         temp = temp.next;
+    //         len++;
+    //     }
+    //     return len;
+    // }
 
-    public ListNode giveTheEdge(ListNode head, int extra) {
+    // public ListNode giveTheEdge(ListNode head, int extra) {
 
-        while(extra > 0){
-            head = head.next;
-            extra--;
-        }
+    //     while(extra > 0){
+    //         head = head.next;
+    //         extra--;
+    //     }
 
-        return head;
-    }
+    //     return head;
+    // }
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
-        int len1 = getLength(headA);
-        int len2 = getLength(headB);
+        // int len1 = getLength(headA);
+        // int len2 = getLength(headB);
 
         //we have to find starting point of both the linkedlists so that we can easily find the intersection point
         ListNode curA = headA;
         ListNode curB = headB;
 
-        if(len1 > len2) {
-            curA = giveTheEdge(headA, len1 - len2);
-        } else {
-            curB = giveTheEdge(headB, len2 - len1);
-        }
+        // if(len1 > len2) {
+        //     curA = giveTheEdge(headA, len1 - len2);
+        // } else {
+        //     curB = giveTheEdge(headB, len2 - len1);
+        // }
 
+        // while(curA != curB) {
+        //     curA = curA.next;
+        //     curB = curB.next;
+        // }
+
+        //2nd Approach
         while(curA != curB) {
             curA = curA.next;
             curB = curB.next;
+            if(curA == null && curB == null) return null;
+            if(curA == null) curA = headB;
+            if(curB == null) curB = headA;
         }
 
         return curA;
