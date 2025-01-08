d48904994b303b2f70376584a0d002ce8d7f9f0c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-01-08 20:38:43 +0530

    Time: 35 ms (32.84%), Space: 51.5 MB (55.95%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,52 +1,47 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        //1:- Sort the array
-        //2:- then iterate the array and calculate the sum=-nums[i]
-        //3:- Check for the nums[j]+nums[k]==sum, then return i,j,k else j++,k--;
-        //4:- Check not to include duplicates like nums[i],nums[j],nums[k], CALCULATE DISTINCT PAIRS
 
         Arrays.sort(nums);
-        List<List<Integer>> list=new ArrayList<>();
-        int i=0;
-        int n=nums.length;
-        while(i<n){
-            int target=-nums[i];
-            int j=i+1;
-            int k=n-1;
-            int duplicateI=nums[i];
+        List<List<Integer>> answer = new ArrayList<>();
+        int i = 0;
+        int n = nums.length;
 
-            while(j<k){
-                if(nums[j]+nums[k]==target){
-                    List<Integer> currentArr=new ArrayList<>();
-                    currentArr.add(nums[i]);
-                    currentArr.add(nums[j]);
-                    currentArr.add(nums[k]);
+        while(i < n) {
+            int target = -nums[i];
+            int duplicateI = nums[i];
+            int j = i + 1;
+            int k = n - 1;
 
-                    list.add(new ArrayList<>(currentArr));
+            while(j < k) {
+                if(nums[j] + nums[k] == target) {
 
-                    //Check for duplicates jth and kth elements
-                    while(j<k && nums[j]==currentArr.get(1)){
-                        j++;
-                    }
+                    List<Integer> currSeq = new ArrayList<>();
+                        currSeq.add(nums[i]);
+                        currSeq.add(nums[j]);
+                        currSeq.add(nums[k]);
 
-                    while(k>j && nums[k]==currentArr.get(2)){
-                        k--;
-                    }
-                }else if(nums[j]+nums[k]<target){
-                    j++;
-                }else{
+                        answer.add(new ArrayList<>(currSeq));
+
+                        while (j < k && nums[j] == currSeq.get(1)){
+                            j++;
+                        }
+
+                        while (k > j && nums[k] == currSeq.get(2)) {
+                            k--;
+                        }
+
+                } else if (nums[j] + nums[k] > target) {
                     k--;
+                } else {
+                    j++;
                 }
             }
 
-            //Check for ith duplicate element
-            while(i<n && duplicateI==nums[i]){
+            while(i < n && nums[i] == duplicateI) {
                 i++;
             }
-
         }
-
-        return list;
+        
+        return answer;
     }
 }
