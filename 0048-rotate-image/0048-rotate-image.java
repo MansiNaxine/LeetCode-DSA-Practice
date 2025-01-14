05020db7233ee5dd076cbc877fa5584030695151
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-01-14 22:20:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (22.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,38 +1,27 @@
 class Solution {
     public void rotate(int[][] matrix) {
 
-        int n=matrix.length;
+        int n = matrix.length;
+        int answer[][] = new int[n][n];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                //transpose matrix
-                if(i<j){
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
+        int k = 0;
+
+        while(k<n ){
+            for(int j=0; j<n; j++) {
+                int l = 0;
+                for(int i=n-1; i>=0; i--) {
+                    answer[k][l] = matrix[i][j];
+                    l++;
                 }
+             k++;
             }
         }
-
-        //traverse through each row to swap the elements
-        for(int i=0;i<n;i++){
-            move(matrix[i]);
-        }
         
-    }
-
-    public void move(int[] matrix){
-        int i=0;
-        int j=matrix.length-1;
-
-        while(i<j){
-            int temp=matrix[i];
-            matrix[i]=matrix[j];
-            matrix[j]=temp;
-
-
-            i++;
-            j--;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                matrix[i][j] = answer[i][j];
+            }
         }
     }
 }
