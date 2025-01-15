e704ba8529ed2058f2d2b57183af8d7db043c052
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-01-15 22:23:36 +0530

    Time: 1 ms (85.74%), Space: 42.3 MB (22.72%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> answer = new ArrayList<>();
 
-        List<List<Integer>> answer=new ArrayList<>();
-        List<Integer> firstRow=new ArrayList<>();
+        List<Integer> firstRow = new ArrayList<>();
         firstRow.add(1);
 
         answer.add(new ArrayList<>(firstRow));
-        int middle=0;
-        for(int i=1;i<numRows;i++){
-            List<Integer> currentRow=new ArrayList<>();
-            currentRow.add(1);
 
-            //Logic for middle element
-            for(int j=1;j<=middle;j++){
-                int element=answer.get(i-1).get(j)+answer.get(i-1).get(j-1);
+        int middleElement = 0;
+
+        for(int i=1; i<numRows; i++) {
+            List<Integer> currentRow = new ArrayList<>();
+            currentRow.add(1);//first element
+
+            //Middle Element
+            for(int j=1; j<=middleElement; j++) {
+                int element = answer.get(i - 1).get(j) + answer.get(i - 1).get(j - 1);
                 currentRow.add(element);
             }
 
-            currentRow.add(1);
+            middleElement++;
+            currentRow.add(1);//last element
             answer.add(new ArrayList<>(currentRow));
-            middle++;
-        }
 
-        return answer;
+        }
         
+       return answer; 
     }
 }
