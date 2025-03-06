16532da00e2c3f1bcb3359001eabd9a98228cca1
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-06 14:37:19 +0530

    Time: 0 ms (100%), Space: 57.6 MB (21.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,45 +1,21 @@
 class Solution {
+
     public void rotate(int[] nums, int k) {
-        
-        k=k%nums.length;
-
-        // int r=0;
-        // //first place the elements fron kth index to rotateArray array
-        // for(int i=nums.length-k;i<nums.length;i++){
-        //     rotateArray[r]=nums[i];
-        //     r++;
-        // }
-        // //shift all the elements in nums array from 0th index to last i.e. after kth index
-        // for(int i=nums.length-1;i>=k;i--){
-        //     nums[i]=nums[i-k];
-        // }
-
-        // //copy elements from rotateArray
-        // for(int i=0;i<k;i++){
-        //     nums[i]=rotateArray[i];
-        // }
-
-        //Second Reverse approach
-        int[] reverseArray1=reverseArray(0,nums.length-1,nums);
-        int[] reverseArray2=reverseArray(0,k-1,nums);
-        int[] reverseArray3=reverseArray(k,nums.length-1,nums);
-
-
-
-
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
     }
 
-    public int[] reverseArray(int i,int j,int[] arr){
+    public void reverse(int[] nums, int i, int j) {
 
-        while(i<j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-
+        while(i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
             i++;
             j--;
         }
-
-        return arr;
     }
 }
