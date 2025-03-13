04bd2a3b0022fb9359f1792c2be2da51505dcead
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-13 15:12:37 +0530

    Time: 101 ms (60.3%), Space: 66.7 MB (80.73%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,41 +1,35 @@
 class NumMatrix {
 
-    int pre_sum[][]=new int[200][200];
+        int[][] prefix ;
     public NumMatrix(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int n = matrix.length;
+        prefix = new int[200][200];
 
-                if(i==0 && j==0){
-                    pre_sum[i][j]=matrix[i][j];
-                }else if(i==0){
-                    pre_sum[i][j]=pre_sum[i][j-1]+matrix[i][j];
-                }else if(j==0){
-                   pre_sum[i][j]=pre_sum[i-1][j]+matrix[i][j]; 
-                }else{
-                    pre_sum[i][j]=pre_sum[i][j-1]+pre_sum[i-1][j]-pre_sum[i-1][j-1]+matrix[i][j]; 
+       for(int i = 0; i < n; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(i == 0 && j == 0) {
+                    prefix[i][j] = matrix[i][j];
+                } else if (i == 0) {
+                    prefix[i][j] = prefix[i][j - 1] + matrix[i][j];
+                } else if (j == 0) {
+                    prefix[i][j] = prefix[i - 1][j] + matrix[i][j];
+                } else {
+                    prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] + matrix[i][j] - prefix[i - 1][j - 1];
                 }
             }
-        }
+       }
+        
     }
     
     public int sumRegion(int r0, int c0, int r1, int c1) {
 
-        int ans=pre_sum[r1][c1];
-        if(r0>0){
-            ans -=pre_sum[r0-1][c1];
-        }
-         if(c0>0){
-            ans -=pre_sum[r1][c0-1];
-        }
-        if(r0>0 && c0>0){
-            ans +=pre_sum[r0-1][c0-1];
-        }
+        int ans = prefix[r1][c1];
+        if(r0 > 0) ans -= prefix[r0-1][c1];
+        if(c0 > 0) ans -= prefix[r1][c0-1];
+        if(r0 > 0 && c0 > 0) ans += prefix[r0-1][c0-1];
 
         return ans;
-        
     }
 }
 
