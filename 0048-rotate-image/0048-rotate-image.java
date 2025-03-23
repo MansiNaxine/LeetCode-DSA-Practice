9811655edb3ef90ae2b43a28e001a259b5ab0cdc
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-23 19:08:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.47%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,24 +2,35 @@ class Solution {
     public void rotate(int[][] matrix) {
 
         int n = matrix.length;
-        int answer[][] = new int[n][n];
-
-        int k = 0;
-
-            for(int j=0; j<n; j++) {
-                int l = 0;
-                for(int i=n-1; i>=0; i--) {
-                    answer[k][l] = matrix[i][j];
-                    l++;
-                }
-             k++;
-            }
-        
-        
+        //we have to create transform of the given matrix
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                matrix[i][j] = answer[i][j];
+                if(i < j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
             }
+        }
+
+        //then just reverse the ea ch row
+        for(int i=0; i<n; i++) {
+            reverse(matrix[i]);
+        }
+        
+    }
+
+    public void reverse(int matrix[]) {
+        int i = 0;
+        int j = matrix.length - 1;
+
+        while(i < j) {
+            int temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
+
+            i++;
+            j--;
         }
     }
 }
