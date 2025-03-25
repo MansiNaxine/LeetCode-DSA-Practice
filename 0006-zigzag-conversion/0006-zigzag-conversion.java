c60b1ba9029f7e8d5b4991bba8c4678611c9db91
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-25 20:31:53 +0530

    Time: 9 ms (27.35%), Space: 45.3 MB (46.23%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,24 +1,21 @@
 class Solution {
     public String convert(String s, int numRows) {
-        
-        //also u have to check for length 1
+
         if(numRows == 1) return s;
+        
+        List<List<Character>> answer = new ArrayList<>();
 
-        //First take a answerList to store the list of characters
-        List<List<Character>> answerList = new ArrayList<>();
-
-        //will initiate lists of characatrers inside answerList with empty values
         for(int i = 0; i < numRows; i++) {
-            List<Character> currSeq = new ArrayList<>();
-
-            answerList.add(new ArrayList<>(currSeq)); //contains the empty character list each time
+            List<Character> currentRow = new ArrayList<>();
+            
+            answer.add(new ArrayList<>(currentRow));
         }
 
-        int rowIndex = 0;//to put the character at right index
         int turn = 0;
+        int rowIndex = 0;
 
         for(int i = 0; i < s.length(); i++) {
-            answerList.get(rowIndex).add(s.charAt(i));
+            answer.get(rowIndex).add(s.charAt(i));
 
             if (turn == 0) {
                 rowIndex++;
@@ -28,22 +25,20 @@ class Solution {
                 }
             } else {
                 rowIndex--;
-                if(rowIndex == -1) {
+                if (rowIndex == -1) {
                     rowIndex += 2;
                     turn = 0;
                 }
             }
         }
 
-        //now copying all characters from answerList to stringBuilder
-        StringBuilder sb = new StringBuilder("");
-        for(int i = 0; i < answerList.size(); i++) {
-            for(int j = 0; j < answerList.get(i).size(); j++) {
-                sb.append(answerList.get(i).get(j));
+        StringBuilder str = new StringBuilder("");
+        for(int i = 0; i < answer.size(); i++) {
+            for(int j = 0; j < answer.get(i).size(); j++) {
+                str.append(answer.get(i).get(j));
             }
         }
 
-        return sb.toString();
-        
+        return str.toString();
     }
 }
