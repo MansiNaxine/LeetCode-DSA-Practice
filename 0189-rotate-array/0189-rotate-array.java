7cb15ba15c73146e0f66fdcda10ccb7c6f8b5b6a
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-27 13:02:09 +0530

    Time: 0 ms (100%), Space: 57.5 MB (50.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,21 +3,40 @@ class Solution {
 
         int n = nums.length;
         k = k % n;
-        int ans[] = new int[n];
-        int index = 0;
-        for(int i = 0; i < k; i++) {
-            ans[index] = nums[n - k + i];
-            index++;
-        }
+        //BruteForce Approach
+        // int ans[] = new int[n];
+        // int index = 0;
+        // for(int i = 0; i < k; i++) {
+        //     ans[index] = nums[n - k + i];
+        //     index++;
+        // }
 
-        for(int i = 0; i < n-k ; i++) {
-            ans[index] = nums[i];
-            index++;
-        }
+        // for(int i = 0; i < n-k ; i++) {
+        //     ans[index] = nums[i];
+        //     index++;
+        // }
 
-        for(int i = 0; i < n; i++) {
-            nums[i] = ans[i];
-        }
+        // for(int i = 0; i < n; i++) {
+        //     nums[i] = ans[i];
+        // }
+
+        //Optimized Approach
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+
         
+    }
+
+    public void reverse(int nums[], int i, int j) {
+
+         while(i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            i++;
+            j--;
+         }
     }
 }
