c2067577504d9634a9ebd3e5245c8ee83834daa4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-03-27 12:59:26 +0530

    Time: 2 ms (29.36%), Space: 57.1 MB (90.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        //BruteForce Approach
+
         int n = nums.length;
         k = k % n;
+        int ans[] = new int[n];
         int index = 0;
-        int arr[] = new int[n];
-
         for(int i = 0; i < k; i++) {
-            arr[index] = nums[n - k + i];
+            ans[index] = nums[n - k + i];
             index++;
         }
 
-        for(int i = 0; i < (n - k) ; i++) {
-            arr[index] = nums[i];
+        for(int i = 0; i < n-k ; i++) {
+            ans[index] = nums[i];
             index++;
         }
 
-        for(int j = 0; j < n; j++) {
-            nums[j] = arr[j];
+        for(int i = 0; i < n; i++) {
+            nums[i] = ans[i];
         }
         
     }
