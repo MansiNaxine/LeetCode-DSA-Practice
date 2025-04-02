58e24cb44cb2add54281934ebc5d4072f7a359b4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-02 18:15:40 +0530

    Time: 9 ms (27.25%), Space: 45.3 MB (52.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,22 +2,26 @@ class Solution {
     public String convert(String s, int numRows) {
 
         if(numRows == 1) return s;
-        
+
         List<List<Character>> answer = new ArrayList<>();
 
+
+        //Creating empty rows inside answer List initially
         for(int i = 0; i < numRows; i++) {
             List<Character> currentRow = new ArrayList<>();
-            
             answer.add(new ArrayList<>(currentRow));
         }
 
-        int turn = 0;
+        //we will iterate through string
         int rowIndex = 0;
-
+        int turn = 0;
         for(int i = 0; i < s.length(); i++) {
-            answer.get(rowIndex).add(s.charAt(i));
 
-            if (turn == 0) {
+            answer.get(rowIndex).add(s.charAt(i));
+            //add character from string
+
+            //choose the character
+            if(turn == 0) {
                 rowIndex++;
                 if(rowIndex == numRows) {
                     rowIndex -= 2;
@@ -25,20 +29,22 @@ class Solution {
                 }
             } else {
                 rowIndex--;
-                if (rowIndex == -1) {
+                if(rowIndex == -1) {
                     rowIndex += 2;
                     turn = 0;
                 }
             }
         }
 
-        StringBuilder str = new StringBuilder("");
+        StringBuilder sb = new StringBuilder("");
+
         for(int i = 0; i < answer.size(); i++) {
             for(int j = 0; j < answer.get(i).size(); j++) {
-                str.append(answer.get(i).get(j));
+                sb.append(answer.get(i).get(j));
             }
         }
 
-        return str.toString();
+        return sb.toString();
+        
     }
 }
