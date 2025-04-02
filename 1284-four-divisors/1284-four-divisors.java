c0a05545ce62cffb397ed7968547eb562673747e
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-02 12:52:50 +0530

    Time: 27 ms (60.83%), Space: 45.4 MB (18.38%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.java
+++ 1284-four-divisors/1284-four-divisors.java
@@ -2,9 +2,16 @@ class Solution {
 
     public int sumFourDivisors(int[] nums) {
         int n = nums.length;
-        int N = 100001;
+        int N = nums[0];
+        int max = nums[0];
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max,nums[i]);
+        }
+
+        N = max + 1;
         int div[] = new int[N];
         int sumDiv[] = new int[N];
+
 
         for(int i = 1; i < N; i++) {
             for(int j = i; j < N; j+=i) {
