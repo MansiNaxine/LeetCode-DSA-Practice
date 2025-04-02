3b55772578b558ed284a088b1555a3a9872492fb
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-02 17:50:12 +0530

    Time: 1 ms (84.82%), Space: 42.3 MB (24.98%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        
-        List<List<Integer>> answer = new ArrayList<>();
+
+        List<List<Integer>>  answer = new ArrayList<>();
         List<Integer> firstRow = new ArrayList<>();
         firstRow.add(1);
 
-        //firstRow we have added
         answer.add(new ArrayList<>(firstRow));
 
-        //taking integer for calculating middle Element
-        //for the first 0th row it won't have middle element
         int midElement = 0;
 
-        //now we wil iterate from the first row
         for(int i = 1; i < numRows; i++) {
             List<Integer> currentRow = new ArrayList<>();
-            currentRow.add(1);
+            currentRow.add(1); // first column of row
 
-            //adding middle elements
+            //adding middle element logic
             for(int j = 1; j <= midElement; j++) {
-                int element = answer.get(i - 1).get(j - 1) + answer.get(i - 1).get(j);
-                currentRow.add(element);
-            } 
+                int midCalc = answer.get(i - 1).get(j - 1) + answer.get(i - 1).get(j);
+                currentRow.add(midCalc) ;
+            }
 
-            midElement++;
-            //last element should also be equals to 1
-            currentRow.add(1);
+            midElement ++;
+            currentRow.add(1);//last column of first row
             answer.add(new ArrayList<>(currentRow));
         }
 
         return answer;
+        
     }
 }
