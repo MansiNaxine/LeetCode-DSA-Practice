14d39f5f7c5a3fd4ba1e2f5aa8cf42a3d197e1b3
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-03 18:30:09 +0530

    Time: 35 ms (32.62%), Space: 51.5 MB (71.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,51 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
+        
+        //first I will sort the array
         Arrays.sort(nums);
+
+        //first form the answer list
         List<List<Integer>> answer = new ArrayList<>();
         int i = 0;
         int n = nums.length;
 
         while(i < n) {
-            int target = -nums[i];
-            int duplicateI = nums[i];
+            int currentI = nums[i];
+            int findSecondNum = -nums[i];
             int j = i + 1;
             int k = n - 1;
 
+            //binary search to find the second number which is negative of current I
             while(j < k) {
-                if(nums[j] + nums[k] == target) {
+                int sum = nums[j] + nums[k];
+                if(sum == findSecondNum) {
+                    List<Integer> currentList = new ArrayList<>();
+                    //adding all nums of I, J, K
+                    currentList.add(nums[i]);
+                    currentList.add(nums[j]);
+                    currentList.add(nums[k]);
 
-                    List<Integer> currSeq = new ArrayList<>();
-                        currSeq.add(nums[i]);
-                        currSeq.add(nums[j]);
-                        currSeq.add(nums[k]);
+                    while(j < n && nums[j] == currentList.get(1)) {
+                        j++;
+                    }
 
-                        answer.add(new ArrayList<>(currSeq));
+                    while(k >=0 && nums[k] == currentList.get(2)) {
+                        k--;
+                    }
 
-                        while (j < n && nums[j] == currSeq.get(1)){
-                            j++;
-                        }
-
-                        while (k > j && nums[k] == currSeq.get(2)) {
-                            k--;
-                        }
-
-                } else if (nums[j] + nums[k] > target) {
-                    k--;
-                } else {
+                    answer.add(new ArrayList<>(currentList));
+                } else if(sum < findSecondNum) {
                     j++;
+                } else {
+                    k--;
                 }
             }
 
-            while(i < n && nums[i] == duplicateI) {
+            while(i < n && nums[i] == currentI) {
                 i++;
             }
         }
-        
+
         return answer;
     }
 }
