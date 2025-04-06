1caf13219688b72f352b6b7df3c984160a37c8f7
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-06 19:06:42 +0530

    Time: 1 ms (93.35%), Space: 45.6 MB (53.69%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,29 +1,50 @@
 class Solution {
     public int pivotIndex(int[] nums) {
 
-        int n= nums.length;
-        int leftSum = 0;
-        int rightSum = -1;
+    //     int n= nums.length;
+    //     int leftSum = 0;
+    //     int rightSum = -1;
 
-        for(int  i = 0; i < n; i++) {
-            rightSum = findSum(i + 1, nums);
-            if(leftSum == rightSum) {
-                return i;
-            } else {
-                leftSum += nums[i];
-            }
-        }
+    //     for(int  i = 0; i < n; i++) {
+    //         rightSum = findSum(i + 1, nums);
+    //         if(leftSum == rightSum) {
+    //             return i;
+    //         } else {
+    //             leftSum += nums[i];
+    //         }
+    //     }
 
-        return rightSum == 0 ? -1 : rightSum;
+    //     return rightSum == 0 ? -1 : rightSum;
         
+    // }
+
+    // public int findSum(int index, int[] nums) {
+    //     int sum = 0;
+    //     for(int i = index; i < nums.length; i++) {
+    //         sum += nums[i];
+    //     }
+
+    //     return sum;
+    // }
+
+    int n = nums.length;
+
+    int leftSum = 0;
+    int sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        sum += nums[i];
     }
 
-    public int findSum(int index, int[] nums) {
-        int sum = 0;
-        for(int i = index; i < nums.length; i++) {
-            sum += nums[i];
+    for(int i = 0; i < n; i++) {
+        if(leftSum == sum - leftSum - nums[i]) {
+            return i;
         }
 
-        return sum;
+        leftSum += nums[i];
+    }
+
+    return -1;
+
     }
 }
