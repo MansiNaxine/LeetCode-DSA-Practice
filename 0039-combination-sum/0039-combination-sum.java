3714849f514a61c2f76938ee2d9cdf45f68f3fa7
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-22 21:06:54 +0530

    Time: 2 ms (82.67%), Space: 44.7 MB (64.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,29 @@
 class Solution {
-    
-    List<List<Integer>> answer = new ArrayList<>();
+
+    List<List<Integer>>  answer = new ArrayList<>();
     List<Integer> currentSeq = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
         recursion(0, candidates, target);
         return answer;
+        
     }
 
     public void recursion(int start, int[] candidates, int target) {
 
+        //base case
         if(target == 0) {
-            answer.add(new ArrayList<Integer>(currentSeq));
+            answer.add(new ArrayList<>(currentSeq));
             return;
         }
 
-        for(int i=start; i<candidates.length; i++) {
+        //recursion steps
+        for(int i = start; i < candidates.length; i++) {
             if(target >= candidates[i]) {
                 currentSeq.add(candidates[i]);
                 recursion(i, candidates, target - candidates[i]);
+                //backtracking
                 currentSeq.remove(currentSeq.size() - 1);
             }
         }
