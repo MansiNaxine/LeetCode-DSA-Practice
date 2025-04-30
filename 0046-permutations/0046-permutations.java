ed124c8fbbc45aad55f050d15384ea2963790353
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-04-30 10:11:08 +0530

    Time: 1 ms (93.32%), Space: 44.6 MB (64.47%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,34 @@
 class Solution {
 
     List<List<Integer>> answer = new ArrayList<>();
-    
-    public List<List<Integer>> permute(int[] nums) {
+    List<Integer> currSeq = new ArrayList<>();
 
-        recursion(0,nums);
-        return answer;
+    public List<List<Integer>> permute(int[] nums) {
         
+        recursion(nums, 0);
+        return answer;
     }
 
-    public void recursion(int index,  int[] nums){
+    public void recursion(int[] nums, int index) {
+
         //base case
-        if(index == nums.length){
-            List<Integer> currAns = new ArrayList<>();
-            for(int i=0;i<nums.length;i++){
-                currAns.add(nums[i]) ;
-            }
-            answer.add(currAns);
+        if(index == nums.length) {
+            answer.add(new ArrayList<>(currSeq));
             return;
         }
 
         //recursive case
-        for(int i=index;i<nums.length;i++){
-            swap(i, index,nums);
-            recursion(index+1,nums);
-            swap(i, index,nums);
+        for(int i = index; i < nums.length; i++) {
+            currSeq.add(nums[i]);
+            //swap elements
+            int temp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = temp;
+            recursion(nums, index + 1);
+            currSeq.remove(currSeq.size() - 1);
+            temp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = temp;
         }
-        
-    }
-
-    public void swap(int i, int index, int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[index];
-        nums[index] = temp;
     }
 }
