6af580565632aa87c0dd3c214821ec8467cbb070
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-04 14:49:04 +0530

    Time: 2 ms (87.83%), Space: 44.8 MB (75.22%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -16,11 +16,13 @@ class Solution {
 
         //For optimization
         int[] colArr = new int[n];
-        solveNQueensHelper(n, chessBoard, 0, colArr);
+        int[] left_diagonal = new int[2*n - 1];
+        int[] right_diagonal = new int[2*n - 1];
+        solveNQueensHelper(n, chessBoard, 0, colArr, left_diagonal, right_diagonal);
         return answer;
     }
 
-    public void solveNQueensHelper(int n, List<StringBuilder> chessBoard, int row, int[] colArr) {
+    public void solveNQueensHelper(int n, List<StringBuilder> chessBoard, int row, int[] colArr, int[] left_diagonal, int[] right_diagonal) {
 
         //base case
         if(row == n) {
@@ -35,28 +37,35 @@ class Solution {
 
         //recursive case
         for(int j = 0; j < n; j++) {
-            if(isSafe(chessBoard, n,row, j, colArr)) {
+            if(isSafe(chessBoard, n,row, j, colArr, left_diagonal, right_diagonal)) {
                 chessBoard.get(row).setCharAt(j, 'Q');
                 colArr[j] = 1;
-                solveNQueensHelper(n, chessBoard, row + 1, colArr);
+                left_diagonal[row-j+n-1] = 1;
+                right_diagonal[row+j] = 1;
+                solveNQueensHelper(n, chessBoard, row + 1, colArr, left_diagonal, right_diagonal);
                 colArr[j] = 0;
+                left_diagonal[row-j+n-1] = 0;
+                right_diagonal[row+j] = 0;
                 chessBoard.get(row).setCharAt(j, '.');
             }
         }
     }
 
 
-    public boolean isSafe(List<StringBuilder> chessBoard, int n, int row, int col, int[] colArr) {
+    public boolean isSafe(List<StringBuilder> chessBoard, int n, int row, int col, int[] colArr,int[] left_diagonal, int[] right_diagonal) {
 
         //check top left
-        int r = row;
-        int c = col;
+        // int r = row;
+        // int c = col;
 
-        while(r >= 0 && c >= 0) {
-            if(chessBoard.get(r).charAt(c) == 'Q') return false;
-            r--;
-            c--;
-        }
+        // while(r >= 0 && c >= 0) {
+        //     if(chessBoard.get(r).charAt(c) == 'Q') return false;
+        //     r--;
+        //     c--;
+        // }
+
+        //for optimization purpose of left diagonal
+        if(left_diagonal[row-col+n-1] == 1) return false;
 
         //check for top
 
@@ -71,14 +80,17 @@ class Solution {
         if(colArr[col] == 1) return false;
 
         //top right
-        r = row;
-        c = col;
+        // r = row;
+        // c = col;
 
-        while(r >= 0 && c < n) {
-            if(chessBoard.get(r).charAt(c) == 'Q') return false;
-            r--;
-            c++;
-        }
+        // while(r >= 0 && c < n) {
+        //     if(chessBoard.get(r).charAt(c) == 'Q') return false;
+        //     r--;
+        //     c++;
+        // }
+
+        //for optimization purpose of right diagonal
+        if(right_diagonal[row+col] == 1) return false;
 
         return true;
     }
