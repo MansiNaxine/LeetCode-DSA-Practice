22bafaed02de43cb28326ba7d2135766002bd5fe
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-05 20:21:43 +0530

    Time: 1 ms (100%), Space: 47.1 MB (60.68%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,47 +1,26 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
 
-        int n=nums.length;
+        int n = nums.length;
+        int[] answer = new int[n];
 
-        //1.BruteForce Approach
+        int i = 0; 
+        int j = n - 1;
+        int index = n - 1;
 
-        // for(int i=0;i<n;i++){
-        //     nums[i] *= nums[i];
-        // }
+        while(i <= j) {
+           if(Math.abs(nums[i]) < Math.abs(nums[j])) {
+            answer[index] = nums[j] * nums[j];
+            j--;
+           } else {
+            answer[index] = nums[i] * nums[i];
+            i++;
+           }
 
-        // for(int i=0;i<n-1;i++){
-
-        //     for(int j=0;j<n-1-i;j++){
-
-        //         if(nums[j]>=nums[j+1]){
-
-        //             int temp=nums[j];
-        //             nums[j]=nums[j+1];
-        //             nums[j+1]=temp;
-        //         }
-        //     }
-        // }
-
-        //2.Optimized Approach
-        int answer[]=new int[n];
-        int index=n-1;
-        int i=0;
-        int j=n-1;
-
-        while(i<=j){
-
-            if(Math.abs(nums[i])>=Math.abs(nums[j])){
-                answer[index]=nums[i]*nums[i];
-                i++;
-            }else{
-                answer[index]=nums[j]*nums[j];
-                j--;
-            }
-
-            index--;
-
+           index--;
         }
-        
+
         return answer;
+        
     }
 }
