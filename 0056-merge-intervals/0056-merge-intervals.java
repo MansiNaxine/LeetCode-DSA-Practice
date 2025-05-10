6169a92e2e041a91fdc549211fca7416620a246a
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-10 15:50:35 +0530

    Time: 13 ms (7.08%), Space: 48.9 MB (6.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,66 +1,48 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        //first we have to take arrayList then we can apply custom comparator
+        int n = intervals.length;
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
 
-        //1.Copy elements from 2D array into ArrayList
+        for(int i = 0; i < n; i++) {
+            ArrayList<Integer> currSeq = new ArrayList<>();
+            currSeq.add(intervals[i][0]);
+            currSeq.add(intervals[i][1]);
 
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-
-        int n= intervals.length;
-
-        for(int i=0;i<n;i++){
-
-            ArrayList<Integer> currList=new ArrayList<>();
-            currList.add(intervals[i][0]);
-            currList.add(intervals[i][1]);
-
-            list.add(new ArrayList<>(currList));
+            arr.add(new ArrayList<>(currSeq));
         }
 
-        //2.Sort ArrayList based on first element
+        //apply custom comparator
+        Collections.sort(arr, (x, y) -> Integer.compare(x.get(0), y.get(0)));
 
-        Collections.sort(list,(x,y)->Integer.compare(x.get(0),y.get(0)));
+        ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
+        answer.add(new ArrayList<>(arr.get(0)));
 
-        
+        int Left  = arr.get(0).get(0);
+        int Right = arr.get(0).get(1);
 
-        //Now take left and right and merge it
+        for(int i = 1; i < arr.size(); i++) {
+                
+                if(arr.get(i).get(0) >= Left && arr.get(i).get(0) <= Right) {
 
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+                    answer.get(answer.size()-1).set(1, Math.max(arr.get(i).get(1), Right));
 
+                } else {
+                    answer.add(new ArrayList<>(arr.get(i)));
+                }
 
-        ans.add(new ArrayList<>(list.get(0)));
-
-        int L=ans.get(0).get(0);
-        int R=ans.get(0).get(1);
-
-        for(int i=1;i<list.size();i++){
-
-            int x=list.get(i).get(0);
-            int y=list.get(i).get(1);
-
-            if(x >= L && x <= R){
-
-                ans.get(ans.size()-1).set(1,Math.max(R,y));
-
-            }else{
-
-                ans.add(new ArrayList<>(list.get(i)));
-            }
-
-            L=ans.get(ans.size()-1).get(0);
-            R=ans.get(ans.size()-1).get(1);
-
+                Left  = answer.get(answer.size()-1).get(0);
+                Right = answer.get(answer.size()-1).get(1);
         }
 
-        int[][] answer=new int[ans.size()][2];
-
-        //3.copy elements from list into 2D array
-        for(int i=0;i<ans.size();i++){
-
-            answer[i][0]=ans.get(i).get(0);
-            answer[i][1]=ans.get(i).get(1);
+        int N = answer.size();
+        int[][] answerFinal = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            answerFinal[i][0] = answer.get(i).get(0);
+            answerFinal[i][1] = answer.get(i).get(1);
         }
 
-        return answer;
-        
+        return answerFinal;
+
     }
 }
