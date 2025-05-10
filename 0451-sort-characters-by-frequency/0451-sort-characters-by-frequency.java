e61b4bb5dde9959acbc561af69195f970df5cc60
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-10 16:04:21 +0530

    Time: 6 ms (88.57%), Space: 44.9 MB (89.37%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,55 +1,43 @@
 class Solution {
     public String frequencySort(String s) {
 
-        //1. Form frequency array
-        int freq[]=new int[256];
-        int n=s.length();
+        //we will create frequncy array
+        int[] freq=new int[256];
 
-        for(int i=0;i<n;i++){
-
-            char currentChar=s.charAt(i);
-            int val=currentChar;
-            freq[val]++;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            freq[s.charAt(i)]++;
         }
-
-        //2. Create ArrayList of ArrayList and copy all elements along with their frequencies into list
-
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
-
-        for(int i=0;i<freq.length;i++){
-
-            //if freq is not zero means element needs to add into arr
-            if(freq[i]!=0){
-
-                ArrayList<Integer> currSeq=new ArrayList<Integer>();
+        
+        //First we will take the arrayList
+        ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
+        for(int i = 0; i < 256; i++) {
+            if(freq[i] != 0) {
+                ArrayList<Integer> currSeq = new ArrayList<>();
                 currSeq.add(i);
                 currSeq.add(freq[i]);
 
-                arr.add(new ArrayList<>(currSeq));
+                answer.add(new ArrayList<>(currSeq));
+            }
+        }
+
+        //we have to sort the arrayList
+        Collections.sort(answer,(x, y) -> Integer.compare(x.get(1), y.get(1)));
+        Collections.reverse(answer);
+
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < answer.size(); i++) {
+            int ch = answer.get(i).get(0);
+            int fr = answer.get(i).get(1);
+
+            while(fr > 0) {
+                str.append((char)ch);
+                fr--;
             }
 
         }
 
-        //3. Sort the list using Collections.sort
-        Collections.sort(arr,(x,y)->Integer.compare(x.get(1),y.get(1)));
-        Collections.reverse(arr);
-
-        //4. Create StringBuilder and append the characters according to frequency
-
-        StringBuilder sb=new StringBuilder("");
-
-        for(int i=0;i<arr.size();i++){
-
-            int fre=arr.get(i).get(1);
-            char ch=(char)(int)arr.get(i).get(0);
-
-            while(fre>0){
-                sb.append(ch);
-                fre--;
-            }
-        }
-
-        return sb.toString();
+        return str.toString();
 
     }
 }
