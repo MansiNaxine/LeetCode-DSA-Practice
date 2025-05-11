539ca1255b66ae5c9b8bf19e93169f5fdeb68d0f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-11 10:53:40 +0530

    Time: 13 ms (7.06%), Space: 48.6 MB (6.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        //first we have to take arrayList then we can apply custom comparator
-        int n = intervals.length;
+        
         ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        int n = intervals.length;
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             ArrayList<Integer> currSeq = new ArrayList<>();
             currSeq.add(intervals[i][0]);
             currSeq.add(intervals[i][1]);
@@ -12,31 +12,28 @@ class Solution {
             arr.add(new ArrayList<>(currSeq));
         }
 
-        //apply custom comparator
         Collections.sort(arr, (x, y) -> Integer.compare(x.get(0), y.get(0)));
 
         ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
         answer.add(new ArrayList<>(arr.get(0)));
 
-        int Left  = arr.get(0).get(0);
+        int Left = arr.get(0).get(0);
         int Right = arr.get(0).get(1);
 
-        for(int i = 1; i < arr.size(); i++) {
-                
-                if(arr.get(i).get(0) >= Left && arr.get(i).get(0) <= Right) {
+        for(int i = 1 ; i < arr.size(); i++) {
+            if (arr.get(i).get(0) >= Left && arr.get(i).get(0) <= Right) {
+                answer.get(answer.size() - 1).set(1, Math.max(arr.get(i).get(1), Right));
+            } else {
+                answer.add(new ArrayList<>(arr.get(i)));
+            }
 
-                    answer.get(answer.size()-1).set(1, Math.max(arr.get(i).get(1), Right));
-
-                } else {
-                    answer.add(new ArrayList<>(arr.get(i)));
-                }
-
-                Left  = answer.get(answer.size()-1).get(0);
-                Right = answer.get(answer.size()-1).get(1);
+            Left = answer.get(answer.size() - 1).get(0);
+            Right = answer.get(answer.size() - 1).get(1);
         }
 
         int N = answer.size();
-        int[][] answerFinal = new int[N][2];
+        int answerFinal[][] = new int[N][2];
+
         for(int i = 0; i < N; i++) {
             answerFinal[i][0] = answer.get(i).get(0);
             answerFinal[i][1] = answer.get(i).get(1);
