746757dcfc6dbfb7db149ea03e3c328a7353595b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-11 11:12:59 +0530

    Time: 5 ms (91.2%), Space: 44.9 MB (92.62%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,40 +1,42 @@
 class Solution {
     public String frequencySort(String s) {
-
-        //we will create frequncy array
-        int[] freq=new int[256];
-
+        
+        //first for the frequence array
         int n = s.length();
+        int freq[] = new int[256];
+
         for(int i = 0; i < n; i++) {
             freq[s.charAt(i)]++;
         }
-        
-        //First we will take the arrayList
+
+        //we will for the arrayList and we will include characters and their frequnecy to sort it later
         ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
+
         for(int i = 0; i < 256; i++) {
             if(freq[i] != 0) {
+
                 ArrayList<Integer> currSeq = new ArrayList<>();
+
                 currSeq.add(i);
                 currSeq.add(freq[i]);
 
                 answer.add(new ArrayList<>(currSeq));
             }
         }
-
-        //we have to sort the arrayList
-        Collections.sort(answer,(x, y) -> Integer.compare(x.get(1), y.get(1)));
+        
+        //use Custom comparator to sort the list based on freq
+        Collections.sort(answer, (x, y) -> Integer.compare(x.get(1), y.get(1)));
         Collections.reverse(answer);
 
         StringBuilder str = new StringBuilder();
         for(int i = 0; i < answer.size(); i++) {
-            char ch = (char)(int)(answer.get(i).get(0));
+            char ch = (char)(int)answer.get(i).get(0);
             int fr = answer.get(i).get(1);
 
             while(fr > 0) {
                 str.append(ch);
                 fr--;
             }
-
         }
 
         return str.toString();
