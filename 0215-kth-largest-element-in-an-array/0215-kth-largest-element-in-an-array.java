3ad8f474d170c6840d3d02dfd86c5ea496391a7e
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-11 17:45:25 +0530

    Time: 2230 ms (5.04%), Space: 61.9 MB (15.4%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,49 +1,48 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        return quickSort(nums, n - k, 0, n-1);
         
+        //solve by using quick sort
+        int n = nums.length;
+        return quickSort(nums, n-k, 0, n-1);
     }
 
     public int quickSort(int[] nums, int index, int start, int end) {
-        
-        //base index
-        if(start == end) {
+
+        //base case
+        if(start >= end) {
             return nums[start];
         }
-
         //recursive case
         int k = pivot(nums, start, end);
 
         if(k == index) {
             return nums[k];
-        } else if (k < index) {
-            return quickSort(nums, index, k + 1, end);
-        } else {
+        } else if (k > index) {
             return quickSort(nums, index, start, k - 1);
+        } else {
+            return quickSort(nums, index, k + 1, end);
         }
-    }
 
+    }
 
     public int pivot(int[] nums, int start, int end) {
 
-        int pivot = nums[end];
-        int  i = start;
+        int pivotEle = nums[end];
+        int i = start;
 
         for(int j = start; j <= end-1; j++) {
-            if(nums[j] < pivot) {
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
+            if(nums[j] < pivotEle) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
 
                 i++;
             }
-        }
+        } 
 
-        int temp = nums[i];
-        nums[i] = nums[end];
-        nums[end] = temp;
-
+        int temp = nums[end];
+        nums[end] = nums[i];
+        nums[i] = temp;
 
         return i;
     }
