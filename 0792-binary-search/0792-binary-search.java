ddda648309f88a9e3651f78509650368aa962b2c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-13 14:37:58 +0530

    Time: 0 ms (100%), Space: 45.2 MB (99.03%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
 
+        int n = nums.length;
         int start = 0;
-        int end = nums.length - 1;
+        int end = n - 1;
 
-        while(start <= end){
-            int mid = (start + end)/2;
-
-            if(nums[mid] == target){
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            }
-            else if(nums[mid] < target){
+            } else if(nums[mid] < target) {
                 start = mid + 1;
-            }
-            else{
+            } else {
                 end = mid - 1;
             }
-            
         }
 
         return -1;
