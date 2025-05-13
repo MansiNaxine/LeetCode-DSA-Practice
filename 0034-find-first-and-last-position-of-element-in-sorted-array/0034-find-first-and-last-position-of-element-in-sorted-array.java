83eafd6c05da2ea3ec9296a70bb526f1ecb621db
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-13 16:09:50 +0530

    Time: 0 ms (100%), Space: 46.1 MB (29.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first=findFirst(nums,target,true);
-        int last=findFirst(nums,target,false);
+        
+        //to find the first and last position we have to calculate upper and lower bound
+        int n = nums.length;
+        int answer[] = {-1, -1};
+        int start = 0;
+        int end = n - 1;
 
-        return new int[]{first,last};
-    }
-
-    public int findFirst(int[] nums,int target,boolean searchForFirsIndex){
-        int ans=-1;
-        int start=0;
-        int end=nums.length-1;
-
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            if(target<nums[mid]){
-                end=mid-1;
-            }else if(target>nums[mid]){
-                start=mid+1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] >= target) {
                 
-            }else{
-                //potential ans
-                ans=mid;
-                if(searchForFirsIndex){
-                    end=mid-1;
-
-                }else{
-                    start=mid+1;
+                end = mid - 1;
+                if(nums[mid] == target) {
+                    answer[0] = mid;
                 }
+            } else {
+                start = mid + 1;
             }
         }
-        return ans;
-    }
 
-   
-} 
+        start = 0;
+        end = n - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] <= target) {
+                start = mid + 1;
+                if(nums[mid] == target) {
+                    answer[1] = mid;
+                }
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
