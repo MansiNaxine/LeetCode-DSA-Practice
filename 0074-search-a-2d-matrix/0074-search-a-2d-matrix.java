dfe25f587f5774f9afe0dbf71d02cc17b19bcb62
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-17 19:01:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (80.97%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,22 +1,41 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
+        //we have to find the solution in O(log (m * n))
         int m = matrix.length;
         int n = matrix[0].length;
+        int i = 0; 
+        int j = n - 1;
 
-        int i = 0;
-        int j = n-1;
+        //first get the row by performing binary search on last column
+        int row = 0;
+        int start = 0;
+        int end = m - 1;
+        if(m > 1) {
+            while(start <= end) {
+                int mid = (start + end) / 2;
+                 if(matrix[mid][n - 1] >= target) {
+                    row = mid; 
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+             }
+        } 
 
-        while(i<m && j >=0){
+        //row = (row == -1) ? 0 : row;
+        
 
-            if(matrix[i][j] == target){
+        start = 0;
+        end = n - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(matrix[row][mid] == target) {
                 return true;
-            }
-            else if(matrix[i][j] < target){
-                i++;
-            }
-            else{
-                j--;
+            } else if (matrix[row][mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
 
