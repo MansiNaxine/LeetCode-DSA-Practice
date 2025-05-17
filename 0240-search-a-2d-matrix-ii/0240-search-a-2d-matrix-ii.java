e939386cd176b4d1c0e344233e4ce211e384e766
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-17 18:28:12 +0530

    Time: 6 ms (47.35%), Space: 46.1 MB (38.23%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
+
+        //apply staircase search
+        int i = 0;
         int m = matrix.length;
         int n = matrix[0].length;
+        int j = n - 1;
 
-        int i = 0;
-        int j = n-1;
-
-        while(i<m && j>=0){
-            if(matrix[i][j] == target){
+        while(i < m && j >= 0) {
+            if(matrix[i][j] == target) {
                 return true;
-            }
-            else if(matrix[i][j] < target){
+            } else if(matrix[i][j] > target) {
+                j--;
+            } else {
                 i++;
             }
-            else{
-                j--;
-            }
         }
-
+        
         return false;
     }
 }
