474f4f60d18778a140a5e308a08a07df0388db3d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-21 08:41:59 +0530

    Time: 1 ms (86.19%), Space: 41 MB (42.45%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -5,17 +5,16 @@ class Solution {
         long end = x;
         int answer = 0;
 
-        while( start <= end) {
-            long mid = (start + end) / 2;
-            if(mid * mid  <= x) {
-                answer = (int) mid;
-                start = mid + 1;//we have to check highest one sqrt which is <= x
+        while(start <= end) {
+            long mid = (start + end)/2;
+            if(mid * mid <= x) {
+                answer = (int)mid;
+                start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-
-        return answer;
         
+        return answer;
     }
 }
