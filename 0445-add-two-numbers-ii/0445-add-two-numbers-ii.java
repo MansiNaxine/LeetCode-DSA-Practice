9cc18d682f975e00173b3af82b43332e01736190
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-27 20:07:04 +0530

    Time: 2 ms (60.35%), Space: 44.5 MB (72.06%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -9,47 +9,23 @@
  * }
  */
 class Solution {
-
-    //reverseFunction
-    public ListNode reverse(ListNode head) {
-        
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = curr.next;
-
-        while(curr != null) {
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-
-            if(next != null) {
-                next = next.next;
-            }
-        }
-
-        return prev;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        l1 = reverse(l1);
-        l2 = reverse(l2);
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
 
         ListNode i = l1;
         ListNode j = l2;
 
-        int carry = 0;
-
         ListNode aH = null;
         ListNode aT = null;
+        int carry = 0;
 
-        while ( i != null && j != null) {
-
+        while(i != null && j != null) {
             int sum = i.val + j.val + carry;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(sum % 10);
-
-            if (aH == null && aT == null) {
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(aH == null && aT == null) {
                 aH = newNode;
                 aT = newNode;
             } else {
@@ -59,17 +35,13 @@ class Solution {
 
             i = i.next;
             j = j.next;
-
         }
 
-        while (i != null) {
-
+        while(i != null) {
             int sum = i.val + carry;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(sum % 10);
-
-            if (aH == null && aT == null) {
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(aH == null && aT == null) {
                 aH = newNode;
                 aT = newNode;
             } else {
@@ -80,13 +52,11 @@ class Solution {
             i = i.next;
         }
 
-        while (j != null) {
+        while(j != null) {
             int sum = j.val + carry;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(sum % 10);
-
-            if (aH == null && aT == null) {
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(aH == null && aT == null) {
                 aH = newNode;
                 aT = newNode;
             } else {
@@ -97,20 +67,29 @@ class Solution {
             j = j.next;
         }
 
-        //take a case where both lists are like below
-        //l1 -> 9 9 9
-        //l2 -> 9 9 9
-        //at last there will carry with 1
-
-        if (carry > 0) {
-
+        if(carry > 0) {
             ListNode newNode = new ListNode(carry);
             aT.next = newNode;
             aT = newNode;
         }
 
-        aH = reverse(aH);
-
-        return aH;
+        return reverseList(aH);
+        
     }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while(temp != null) {
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+
+        return prev;
+    }
+
+
 }
