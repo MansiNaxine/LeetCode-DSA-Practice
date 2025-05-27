0711282a603da44f8a56b35219639f2b5615ec26
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-27 18:18:06 +0530

    Time: 7 ms (37.77%), Space: 69.2 MB (12.99%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,62 +10,52 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode mid=getMid(head);
-        ListNode headSecond=reverseList(mid);
-        ListNode reverseBack=headSecond;
 
-        //compare login will be here
-        while(head!=null && headSecond!=null){
-            if(head.val!=headSecond.val){
-                break;
-            }else{
-                head=head.next;
-                headSecond=headSecond.next;
+        //first get the middle element in even case we need first middle element
+        //in odd case we will get the correct one
+        ListNode middle = getMiddleElement(head);
+        ListNode nextPart = middle.next;
+        middle.next = null;
+
+        //we have to reverse the nextPart
+        nextPart = reverseList(nextPart);
+        ListNode i = head;
+        ListNode j = nextPart;
+
+        while(i != null && j != null) {
+            if(i.val != j.val) {
+                return false;
             }
+
+            i = i.next;
+            j = j.next;
         }
 
-        
-
-        reverseList(reverseBack);
-
-        if(head==null || headSecond==null){
-            return true;
-        }
-
-
-        return false;
-
+        return true;
         
     }
 
-    public ListNode reverseList(ListNode head){
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
 
-       
-        ListNode prev=null;
-        ListNode current=head;
-        ListNode next=current.next;
-
-        while(current!=null){
-            current.next=prev;
-            prev=current;
-            current=next;
-            if(next!=null){
-                next=next.next;
-            }
+        while(temp != null) {
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
         }
-
-        
 
         return prev;
     }
 
-    public ListNode getMid(ListNode head){
-        ListNode fast=head;
-        ListNode slow=head;
+    public ListNode getMiddleElement(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        while(fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
         return slow;
