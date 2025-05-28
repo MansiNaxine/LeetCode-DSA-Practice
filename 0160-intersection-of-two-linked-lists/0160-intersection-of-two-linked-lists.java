66a6d71029e2d8344a1a2d09690bc05a15b540ec
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-28 09:37:25 +0530

    Time: 1 ms (99.87%), Space: 48.3 MB (83.8%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,58 +10,20 @@
  * }
  */
 public class Solution {
-
-    // public int getLength(ListNode head) {
-        
-    //     int len = 0;
-    //     ListNode temp = head;
-    //     while(temp != null) {
-    //         temp = temp.next;
-    //         len++;
-    //     }
-    //     return len;
-    // }
-
-    // public ListNode giveTheEdge(ListNode head, int extra) {
-
-    //     while(extra > 0){
-    //         head = head.next;
-    //         extra--;
-    //     }
-
-    //     return head;
-    // }
-
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null && headB == null) return null;
 
-        // int len1 = getLength(headA);
-        // int len2 = getLength(headB);
+        ListNode i = headA;
+        ListNode j = headB;
 
-        //we have to find starting point of both the linkedlists so that we can easily find the intersection point
-        ListNode curA = headA;
-        ListNode curB = headB;
-
-        // if(len1 > len2) {
-        //     curA = giveTheEdge(headA, len1 - len2);
-        // } else {
-        //     curB = giveTheEdge(headB, len2 - len1);
-        // }
-
-        // while(curA != curB) {
-        //     curA = curA.next;
-        //     curB = curB.next;
-        // }
-
-        //2nd Approach
-        while(curA != curB) {
-            curA = curA.next;
-            curB = curB.next;
-            if(curA == null && curB == null) return null;
-            if(curA == null) curA = headB;
-            if(curB == null) curB = headA;
+        while(i != j) { 
+                i = i == null ? headB : i.next;
+                j = j == null ? headA : j.next;
         }
 
-        return curA;
+        return i;
 
+        
     }
+
 }
