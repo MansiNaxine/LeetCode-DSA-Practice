c877fa9cf239b9b5c2bc879b404a87610c1b2ffa
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-05-29 23:01:53 +0530

    Time: 45 ms (26.16%), Space: 46.6 MB (22.62%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+
+        //first form the stack and store arrayList inside that which will contain ASCII value of character + frequency of that same character
+
+        Stack<ArrayList<Integer>> stack = new Stack<>();
+
+        //iterate through string
+        for(int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty()) {
+                ArrayList<Integer> curr = new ArrayList<>();
+                curr.add((int)s.charAt(i));
+                curr.add(1);
+
+                stack.push(new ArrayList<>(curr));
+            } else {
+                ArrayList<Integer> topElement = stack.peek();
+                int topCharacter = topElement.get(0);
+                int topCharFreq = topElement.get(1);
+                if(topCharacter == (int)s.charAt(i)) {
+                    topElement.set(1, topCharFreq+1);
+                } else {
+                    ArrayList<Integer> curr = new ArrayList<>();
+                    curr.add((int)s.charAt(i));
+                    curr.add(1);
+
+                    stack.push(new ArrayList<>(curr));
+                }
+
+                
+            }
+
+            if(stack.peek().get(1) == k) stack.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()) {
+            ArrayList<Integer> topElement = stack.peek();
+            int freq = topElement.get(1);
+            while(freq > 0) {
+                sb.append((char)(int)topElement.get(0));
+                freq--;
+            }
+            stack.pop();
+            
+        }
+
+        return sb.reverse().toString();
+        
+    }
+}
