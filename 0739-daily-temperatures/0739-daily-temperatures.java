b193d3a56adce580961b7ba4ce643865e3957f6c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-06-11 09:51:17 +0530

    Time: 76 ms (33.51%), Space: 62.9 MB (30.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -6,14 +6,7 @@ class Solution {
 
         Stack<Integer> st = new Stack<>();
 
-        //first insert all zeroes in resultant array
         for(int i = 0; i < n; i++) {
-            result[i] = 0;
-        }
-
-        //now push the elements in array beased on nextGreater
-        for(int i = 0; i < n; i++) {
-
             while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
                 result[st.peek()] = i - st.pop();
             }
