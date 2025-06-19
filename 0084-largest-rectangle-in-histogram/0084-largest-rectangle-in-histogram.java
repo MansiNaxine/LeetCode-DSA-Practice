51798a468ebd29a65627e34e8336f8c5d0d85172
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-06-19 17:13:53 +0530

    Time: 78 ms (38.11%), Space: 58.6 MB (12.69%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,17 +1,16 @@
 class Solution {
 
-    public int[] previousSmallelement(int[] arr) {
+    public int[] previousSmallElement(int[] arr) {
         int n = arr.length;
         int[] result = new int[n];
+
+        Stack<Integer> st = new Stack<>();
 
         for(int i = 0; i < n; i++) {
             result[i] = -1;
         }
 
-        Stack<Integer> st = new Stack<>();
-
         for(int i = n-1; i >= 0; i--) {
-
             while(!st.isEmpty() && arr[i] < arr[st.peek()]) {
                 result[st.pop()] = i;
             }
@@ -22,19 +21,17 @@ class Solution {
         return result;
     }
 
-    public int[] nextSmallElement(int[] arr) {
-
+     public int[] nextSmallElement(int[] arr) {
         int n = arr.length;
         int[] result = new int[n];
+
+        Stack<Integer> st = new Stack<>();
 
         for(int i = 0; i < n; i++) {
             result[i] = n;
         }
 
-        Stack<Integer> st = new Stack<>();
-
         for(int i = 0; i < n; i++) {
-
             while(!st.isEmpty() && arr[i] < arr[st.peek()]) {
                 result[st.pop()] = i;
             }
@@ -44,16 +41,17 @@ class Solution {
 
         return result;
     }
+
     public int largestRectangleArea(int[] heights) {
 
         int n = heights.length;
         int maxArea = 0;
 
+        int[] prevSmall = previousSmallElement(heights);
         int[] nextSmall = nextSmallElement(heights);
-        int[] previousSmall = previousSmallelement(heights);
 
         for(int i = 0; i < n; i++) {
-            int area = heights[i] * (nextSmall[i] - previousSmall[i] - 1);
+            int area = heights[i] * (nextSmall[i] - prevSmall[i] - 1);
 
             maxArea = Math.max(maxArea, area);
         }
