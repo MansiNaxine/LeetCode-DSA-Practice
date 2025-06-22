61845b3a443952d82c2774e5ced4048ed5edf64b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-06-22 14:47:56 +0530

    Time: 4 ms (27.19%), Space: 46.3 MB (82.07%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -2,27 +2,55 @@ class Solution {
     public int longestOnes(int[] nums, int k) {
 
         int answer = 0;
-        Deque<Integer> q = new LinkedList<>();
+        //Deque<Integer> q = new LinkedList<>();
         int zero = 0;
 
-        for(int i = 0; i < nums.length; i++) {
+        // for(int i = 0; i < nums.length; i++) {
 
-            q.addLast(nums[i]);
-            if(nums[i] == 0) {
+        //     q.addLast(nums[i]);
+        //     if(nums[i] == 0) {
+        //         zero++;
+        //     }
+
+        //     while(zero > k && !q.isEmpty()) {
+
+        //         if(q.peekFirst() == 0) {
+        //             zero--;
+        //         }
+        //         q.removeFirst();
+        //     }
+
+        //     answer = Math.max(answer, q.size());
+        // }
+
+        //with optimized space complexity
+    int i = 0; 
+    int j = 0;
+    int n = nums.length;
+
+    while(i < n && j < n) {
+       
+
+        if(zero <= k) {
+             if(nums[j] == 0) {
                 zero++;
+             }
+            
+            j++;
+
+        } else {
+            if(nums[i] == 0) {
+                zero--;
             }
-
-            while(zero > k && !q.isEmpty()) {
-
-                if(q.peekFirst() == 0) {
-                    zero--;
-                }
-                q.removeFirst();
-            }
-
-            answer = Math.max(answer, q.size());
+            i++;
         }
 
+        if(zero <= k) {
+
+            answer = Math.max(answer, (j - i));
+        }
+        
+    }
         return answer;
         
     }
