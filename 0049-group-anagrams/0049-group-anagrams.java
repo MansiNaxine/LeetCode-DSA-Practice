7013aa2a5a18a3bd46c8777d09651bacc5981390
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-07-01 15:43:17 +0530

    Time: 8 ms (40.2%), Space: 48.1 MB (44.79%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        int n = strs.length;
+        List<List<String>> answer = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            String str = strs[i];
+            str = sortString(strs[i]);
+
+            if(map.containsKey(str)) {
+                map.get(str).add(strs[i]);
+            } else {
+                List<String> arr = new ArrayList<>();
+                arr.add(strs[i]);
+                map.put(str, new ArrayList<>(arr));
+            }
+        }
+
+        for(String key: map.keySet()) {
+            List<String> arr = map.get(key);
+            answer.add(new ArrayList<>(arr));
+        }
+
+        return answer;
+        
+    }
+
+    public String sortString(String str) {
+
+        char[] ch = str.toCharArray();
+        Arrays.sort(ch);
+
+        String str1 = new String(ch);
+
+        return str1;
+    }
+}
