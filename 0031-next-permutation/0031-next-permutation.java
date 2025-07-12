29aed7ccb3695192f11e6662695989528c34939e
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-07-12 21:08:58 +0530

    Time: 0 ms (100%), Space: 43.5 MB (11.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,6 +1,6 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        
+
         int n = nums.length;
         int i = n - 2;
 
@@ -8,9 +8,9 @@ class Solution {
             i--;
         }
 
-        if(i >= 0) {
+        if( i >= 0) {
             int j = n - 1;
-            while(j >=0 && nums[j] <= nums[i]) {
+            while(j > 0 && nums[j] <= nums[i]) {
                 j--;
             }
 
@@ -21,6 +21,7 @@ class Solution {
 
         int k = i + 1;
         int m = n - 1;
+
         while(k < m) {
             int temp = nums[k];
             nums[k] = nums[m];
@@ -28,5 +29,6 @@ class Solution {
             k++;
             m--;
         }
+        
     }
 }
