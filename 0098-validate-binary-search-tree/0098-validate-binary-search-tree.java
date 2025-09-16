cb4ebe7a20532cd790dff76e85a9d0ef9a0ce1e9
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-09-16 12:33:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (59.04%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,27 +14,42 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
+    // List<Integer> list = new ArrayList<>();
 
-    void recursion(TreeNode root) {
-        // base case
-        if(root == null) return;
+    // void recursion(TreeNode root) {
+    //     // base case
+    //     if(root == null) return;
 
-        // recursive case
-        recursion(root.left);
-        list.add(root.val);
-        recursion(root.right);
+    //     // recursive case
+    //     recursion(root.left);
+    //     list.add(root.val);
+    //     recursion(root.right);
+    // }
+
+    boolean recursion(TreeNode root, long start, long end) {
+        if(root == null) return true;
+
+        if(root.val < start || root.val > end) return false;
+
+        //(Left, root.val - 1) <---- root.val ----> (root.val + 1, Right); all values should be in this range
+        boolean leftPart = recursion(root.left, start, (long)root.val - 1);
+        boolean rightPart = recursion(root.right, (long)root.val + 1, end);
+
+        return leftPart && rightPart;
+
     }
 
     public boolean isValidBST(TreeNode root) {
         
-        recursion(root);
-        for(int i = 0 ; i < list.size() - 1; i++) {
-            if(list.get(i) >= list.get(i + 1)) {
-                return false;
-            }
-        }
+        // recursion(root);
+        // for(int i = 0 ; i < list.size() - 1; i++) {
+        //     if(list.get(i) >= list.get(i + 1)) {
+        //         return false;
+        //     }
+        // }
 
-        return true;
+        // return true;
+
+        return recursion(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
