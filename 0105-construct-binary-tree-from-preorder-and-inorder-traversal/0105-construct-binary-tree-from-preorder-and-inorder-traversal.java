131684c89e6e0bf0d779a5bb33cdb5c92195e958
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-09-17 13:13:19 +0530

    Time: 3 ms (33.2%), Space: 44.1 MB (87.82%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,59 +14,38 @@
  * }
  */
 class Solution {
+    int index = 0;
+    public TreeNode recursion(int[] preorder, int[] inorder, int start, int end) {
+        //base case
+        if(start > end) {
+            return null;
+        }
+
+        //recursive case
+        
+        int newIndex = -1;
+        for(int i = start; i <= end; i++) {
+            if(inorder[i] == preorder[index]) {
+                newIndex = i;
+                index++;
+                break;
+            }
+
+        }
+
+        TreeNode root = new TreeNode(inorder[newIndex]);
+        root.left = recursion(preorder, inorder, start, newIndex - 1);
+        root.right = recursion(preorder, inorder, newIndex + 1, end);
+
+        return root;
+
+    }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
 
-        // if(preorder.length == 0){
-        //     return null;
-        // }
-
-        // int root=preorder[0];
-        // int index=0;
-
-        // for(int i=0;i<inorder.length;i++){
-        //     if(inorder[i]==root){
-        //         index=i;
-        //     }
-        // }
-
-        // TreeNode node=new TreeNode(root);
-
-        // node.left=buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index));
-        // node.right=buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
-
-        // return node;
-
-        HashMap<Integer,Integer> map=new HashMap<>();
-
-        for(int i=0;i<inorder.length;i++){
-            map.put(inorder[i],i);
-        }
-        int[] index={0};
-        return helper(preorder,inorder,map,0,preorder.length-1,index);
- 
-    }
-
-
-    public TreeNode helper(int[] preOrder, int[] inOrder,HashMap<Integer,Integer> map,int left,int right,int[] index ){
-
-        if(left>right){
-            return null;
-        }
-        int current=preOrder[index[0]];
-        index[0]++;
-
-        TreeNode node=new TreeNode(current);
+        int n = inorder.length;
         
-
-        //leaf node condition
-        if(left==right){
-            return node;
-        }
-        int CurrentIndex=map.get(current);
-        node.left=helper(preOrder,inOrder,map,left,CurrentIndex-1,index);
-        node.right=helper(preOrder,inOrder,map,CurrentIndex+1,right,index);
-
-        return node;
+        return recursion(preorder, inorder, 0, n - 1);
+        
     }
 }
