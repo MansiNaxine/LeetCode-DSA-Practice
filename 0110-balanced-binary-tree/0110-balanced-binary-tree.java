97673e150c4998d1b7df3d266dd60cb4573b3df0
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-09-20 15:32:17 +0530

    Time: 1 ms (23.33%), Space: 44.3 MB (86.91%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,19 +15,57 @@
  */
 class Solution {
 
-    public int recursion(TreeNode root) {
+    class Pair {
+    int num;
+    boolean flag;
 
-        if(root == null) return 0;
-
-        return Math.max(recursion(root.left), recursion(root.right)) + 1;
-    }
-    public boolean isBalanced(TreeNode root) {
-        if (root == null) return true ;
-        int Left = recursion(root.left);
-        int Right = recursion(root.right);
-
-        int answer = Math.abs((recursion(root.left) - recursion(root.right)));
-        
-        return answer > 1 ? false : isBalanced(root.left) && isBalanced(root.right);
+    Pair(int num,boolean flag) {
+        this.num = num;
+        this.flag = flag;
     }
 }
+
+    // public int recursion(TreeNode root) {
+
+    //     if(root == null) return 0;
+
+    //     return Math.max(recursion(root.left), recursion(root.right)) + 1;
+    // }
+    public boolean isBalanced(TreeNode root) {
+        // if (root == null) return true ;
+
+        // int Left = recursion(root.left);
+        // int Right = recursion(root.right);
+
+        // int answer = Math.abs((recursion(root.left) - recursion(root.right)));
+
+        // return answer > 1 ? false : isBalanced(root.left) && isBalanced(root.right);
+
+        Pair answer = isBalancedHelper(root);
+
+        return answer.flag;
+    }
+
+    public Pair isBalancedHelper(TreeNode root) {
+
+        if(root == null) {
+
+            return new Pair(0, true);
+        }
+
+        Pair left = isBalancedHelper(root.left);
+        Pair right = isBalancedHelper(root.right);
+
+        int height = Math.max(left.num, right.num) + 1;
+
+        int diff = Math.abs(left.num - right.num);
+
+        if(diff > 1) {
+            return new Pair(height, false);
+        } else {
+
+            return new Pair(height, left.flag && right.flag);
+        }
+    }
+}
+
