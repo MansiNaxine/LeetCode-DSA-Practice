d0eca52d6f77937faf5ea5666073d3cb96c26729
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-10-01 15:18:53 +0530

    Time: 0 ms (100%), Space: 43.3 MB (20.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,27 +1,26 @@
 class Solution {
+
     public double myPow(double x, int n) {
-        
-        if(n < 0) {
-            return 1/helper(x, -n);
-        } 
-        return helper(x, n);
+        if(n == 0) return 1.0;
+        int origN = n;
+        double answer = helper(x,(long) n);
+
+        if(origN < 0) {
+            return 1/answer ; 
+        } else {
+            return answer;
+        }
     }
 
-    public double helper(double x, int n) {
-        if(n == 0) return 1.0;
-        // double answer = 1.0;
-        // if(n == 1) {
-        //     return x;
-        // }
-
-        // answer = x * helper(x, n-1);
-        // return answer;
-
-        double partialAns = helper(x, n/2);
-        if(n%2 == 0) {
-            return partialAns * partialAns;
-        } else {
-            return partialAns * partialAns * x;
-        }
+    public double helper(double x, long n) {
+            if(n == 0) return 1.0;
+            //below solution won't work incase of large n values
+            //double answer = x * negativeCaseHelper(x , Math.abs(n) -  1);
+            double partialAns = helper(x, n/2);
+            if(n%2 == 0) {
+                return partialAns * partialAns;
+            } else {
+                return partialAns * partialAns * x;
+            }
     }
 }
