d89904101860b3871777483d64ca97fa40a34177
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-10-03 09:59:34 +0530

    Time: 2 ms (84.5%), Space: 44.5 MB (92.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,34 +1,35 @@
 class Solution {
+
     List<List<Integer>> answer = new ArrayList<>();
     List<Integer> currSeq = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        recursion(0, candidates, target);
-       
+        Arrays.sort(candidates);
+        helper(candidates, target, 0);
         return answer;
-        
     }
 
-    public void recursion(int start, int[] candidates, int target) {
+    public void helper(int[] candidates, int target, int index) {
 
         //base case
+        if(index >= candidates.length) return;
+
         if(target == 0) {
-            List<Integer> ans = new ArrayList<>();
-            for(int i = 0; i < currSeq.size(); i++) {
-                ans.add(currSeq.get(i));
-            }
-            answer.add(new ArrayList<>(ans));
+            answer.add(new ArrayList<>(currSeq));
             return;
         }
 
-         //recursive case
-        for(int i = start; i < candidates.length; i++) {
-            if (target >= candidates[i]) {
-                currSeq.add(candidates[i]);
-                recursion(i, candidates, target-candidates[i]);
-                currSeq.remove(currSeq.size() - 1);
-            }
-        }
+        //recursive case
+        
+            if(candidates[index] <= target) {
+
+                    currSeq.add(candidates[index]);
+                    helper(candidates, target - candidates[index], index);
+                    currSeq.removeLast();
+                    helper(candidates, target, index + 1);
+
+            } 
+        
+        
     }
 }
