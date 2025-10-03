3ec60a0500b2b012f62de11e5a760e0614660cf2
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-10-03 16:11:55 +0530

    Time: 2 ms (99.78%), Space: 43.5 MB (23.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,36 +1,44 @@
 class Solution {
+
     List<List<Integer>> answer = new ArrayList<>();
     List<Integer> currSeq = new ArrayList<>();
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-
+        if(candidates.length <= 1 && target > candidates[0]) return answer;
         Arrays.sort(candidates);
+        
         helper(candidates, target, 0);
         return answer;
+        
     }
 
     public void helper(int[] candidates, int target, int index) {
 
-        int n = candidates.length;
-        //base case
-            if(target == 0) {
-                List<Integer> newCurr = new ArrayList<>();
-                for(int i = 0; i < currSeq.size(); i++) {
-                    newCurr.add(currSeq.get(i));
-                }
-                answer.add(new ArrayList<>(newCurr));
-                return;
-            }
+        //base cases
+
+        if(target == 0) {
+
+            answer.add(new ArrayList<>(currSeq)) ;
+
+            return;
+        }
+
+        if(index >= candidates.length) return;
 
         //recursive case
-        for(int i = index; i < n; i++) {
-            if(i > index && candidates[i] == candidates[i-1]) continue;
-            if(target >= candidates[i]) {
-                currSeq.add(candidates[i]);
-                helper(candidates, target-candidates[i], i+1);
-                currSeq.remove(currSeq.size() - 1);
+        if(candidates[index] <= target) {
+            currSeq.add(candidates[index]);
+            helper(candidates, target - candidates[index], index + 1);
+            currSeq.removeLast();
+
+            //below logic is to remove duplicate elements
+            int newIndex = index + 1;
+            while(newIndex < candidates.length && candidates[newIndex] == candidates[newIndex - 1]) {
+                newIndex++;
             }
+            helper(candidates, target, newIndex);
         }
-            
+
+
     }
 }
