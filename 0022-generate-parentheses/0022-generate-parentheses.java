9f5e62f34cf68b669264cf13917bb905d9dafbc0
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-10-03 10:27:54 +0530

    Time: 1 ms (80.26%), Space: 43 MB (81.65%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,46 +1,35 @@
 class Solution {
-
     List<String> answer = new ArrayList<>();
-    List<Character> currSeq = new ArrayList<>();
+    StringBuilder str = new StringBuilder();
 
     public List<String> generateParenthesis(int n) {
-
-        int sum = 0;
-        int len = 2*n;
-        recursion(sum, len, 0);
+        int len = 2 * n;
+        generateParenthesisHelper(len, 0, 0);
         return answer;
-        
     }
 
-    public void recursion(int sum, int len, int i) {
+    public void generateParenthesisHelper(int length, int index, int sum) {
 
         //base case
-        if(i == len) {
+        if(index == length) {
             if(sum == 0) {
-                StringBuilder sb = new StringBuilder("");
-                for(int k = 0; k < currSeq.size(); k++) {
-                    sb.append(currSeq.get(k));
-                }
-
-                answer.add(sb.toString());
+                answer.add(str.toString());
+                return;
             }
-
             return;
         }
-
+        
         //recursive case
-        if(sum < len/2) {
-            currSeq.add('(');
-            recursion(sum+1, len, i+1);
-            currSeq.remove(currSeq.size() - 1);
+        if(sum <= length/2) {
+            str.append('(');
+            generateParenthesisHelper(length, index + 1, sum + 1);
+            str.deleteCharAt(str.length() - 1);
         }
 
         if(sum > 0) {
-            currSeq.add(')');
-            recursion(sum-1, len, i+1);
-            currSeq.remove(currSeq.size() - 1);
+            str.append(')');
+            generateParenthesisHelper(length, index + 1, sum - 1);
+            str.deleteCharAt(str.length() - 1);
         }
-
-
     }
 }
