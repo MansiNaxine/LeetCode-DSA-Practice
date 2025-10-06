1fb62e19bffff77ee3e3e7ada4922bdf1e43ebb5
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-10-06 10:13:50 +0530

    Time: 1 ms (92.92%), Space: 45.1 MB (5.43%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,32 +1,34 @@
 class Solution {
 
     List<List<Integer>> answer = new ArrayList<>();
-    List<Integer> currSeq = new ArrayList<>();
+    List<Integer> currSeq;
 
     public List<List<Integer>> permute(int[] nums) {
-        
-        recursion(nums, 0);
+
+        helper(nums, 0);
         return answer;
+        
     }
 
-    public void recursion(int[] nums, int index) {
+    public void helper(int[] nums, int index) {
 
         //base case
         if(index == nums.length) {
+            currSeq = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++) {
+                currSeq.add(nums[i]);
+            }
             answer.add(new ArrayList<>(currSeq));
             return;
         }
 
         //recursive case
         for(int i = index; i < nums.length; i++) {
-            currSeq.add(nums[i]);
-            //swap elements
-            int temp = nums[i];
+            int temp1 = nums[i];
             nums[i] = nums[index];
-            nums[index] = temp;
-            recursion(nums, index + 1);
-            currSeq.remove(currSeq.size() - 1);
-            temp = nums[i];
+            nums[index] = temp1;
+            helper(nums, index + 1);
+            int temp = nums[i];
             nums[i] = nums[index];
             nums[index] = temp;
         }
