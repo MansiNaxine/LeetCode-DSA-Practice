efaa8bb54cbf1b0ef7701946824c7c8b8e4f38fb
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-11-10 12:53:02 +0530

    Time: 1 ms (38.28%), Space: 42.3 MB (9.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        if(n == 1) return true;
-        if((n % 2) == 1) return false;
 
-        return (n % 2) == 0 ? isPowerOfTwo(n/2) : false;
+        return n == 1 ? true : ((n > 0) && (n % 2 == 0)) ? isPowerOfTwo(n/2) : false;
         
     }
 }
