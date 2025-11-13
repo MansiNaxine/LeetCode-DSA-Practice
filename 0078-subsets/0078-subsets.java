63051ef998210b04c6986aeed506c9e4e091a985
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-11-13 16:11:17 +0530

    Time: 0 ms (100%), Space: 44.3 MB (5.47%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
 
-        int n = nums.length;
         List<List<Integer>> answer = new ArrayList<>();
-
-        int len = (int) Math.pow(2, n);
-
-        for(int i = 0; i < len; i++) {
-            List<Integer> arr = new ArrayList<>();
-
-            int index = 0;
-            int numIn = i;
-            while(numIn > 0) {
-                int num = numIn % 2;
-                if(num == 1) arr.add(nums[index]);
-                numIn /= 2;
-                index++;
-            }
-
-            answer.add(new ArrayList<>(arr));
-        }
-
+        List<Integer> currSeq = new ArrayList<>();
+        helper(nums, answer, currSeq, 0);
         return answer;
         
+    }
+
+
+    public void helper(int[] nums, List<List<Integer>> answer, List<Integer> currSeq, int index) {
+        //base case
+        if(index >= nums.length) {
+            answer.add(new ArrayList<>(currSeq));
+            return;
+        }
+
+        //recursive case
+        
+        currSeq.add(nums[index]);
+        helper(nums, answer, currSeq, index + 1);
+        currSeq.removeLast();
+        helper(nums, answer, currSeq, index + 1);
+
     }
 }
