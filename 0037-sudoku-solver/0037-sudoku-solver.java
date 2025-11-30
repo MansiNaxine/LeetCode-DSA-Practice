f06953cd12cca1123841b11e3900141d1a16f4fc
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-11-30 13:20:32 +0530

    Time: 1807 ms (5.34%), Space: 42.7 MB (26.58%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,94 +1,49 @@
 class Solution {
     public void solveSudoku(char[][] board) {
 
-        if(solve(board)){
-            display(board);
-        }
-        System.out.println();
-        
+        int count = 0;
+        int col = 0;
+        helper(board);
+        return;
     }
 
-    public static void display(char[][] board){
-        for(char[] arr:board){
-            for(char element:arr){
-                System.out.print(element+" ");
-            }
-            System.out.println();
-        }
-    }
-
-
-    public static boolean solve(char[][] board){
-        int row=-1;
-        int col=-1;
-
-        boolean isEmpty=true;
-
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                if(board[i][j]=='.'){
-                    row=i;
-                    col=j;
-                    isEmpty=false;
-                    break;
-                }
-            }
-
-            if(!isEmpty){
-                break;
-            }
-        }
-
-        if(isEmpty){
-            return true;
-        }
-
-        for(int num=1; num<=9;num++){
-            if(isSafe(board,row,col,num)){
-                board[row][col]=(char)(num+'0');
-
-                if(solve(board)){
-                    return true;
-                }else{
-                    board[row][col]='.';
-                }
-            }
-        }
-
-        return false;
-
-    }
-
-
-    public static boolean isSafe(char[][] board,int row,int col, int num){
-
-        //to check in rows
-        for(int i=0;i<board.length;i++){
-            if(board[row][i]==(char)(num+'0')){
-                return false;
-            }
-        }
-
-        //to check in columns
-        for(int i=0;i<board.length;i++){
-            if(board[i][col]==(char)(num+'0')){
-                return false;
-            }
-        }
-
-        //to check in 3*3 matrix
-        int sqrt=(int)(Math.sqrt(board.length));
-        int rowStart=row-row%3;
-        int colStart=col-col%3;
-
-        for(int i=rowStart;i<rowStart+sqrt;i++){
-            for(int j=colStart;j<colStart+sqrt;j++){
-                if(board[i][j]==(char)(num+'0')){
+    public boolean helper(char[][] board) {
+        // if(col == board.length) {
+        //     return true;
+        // }
+        //recursive case
+        for(int col = 0; col < board.length; col++) {
+            for(int row = 0; row < board.length; row++) {
+                if(board[row][col] == '.') {
+                    for(char validNum = '1'; validNum <= '9'; validNum++) {
+                        if(isValid(board, col, row, validNum)) {
+                            board[row][col] = validNum;
+                            if (helper(board)) return true;
+                            else board[row][col] = '.';
+                        }
+                    }
                     return false;
                 }
-            }
+             }
         }
+        
+        return true;
+    }
 
+    public boolean isValid(char[][] board, int col, int row, char validNum) {
+
+        
+        for( int i = 0;  i < 9; i++) {
+            //check for entire row
+            if(board[row][i] == validNum) return false;
+
+            //check for entire col
+            if(board[i][col] == validNum) return false;
+
+            //check 3 * 3 matrix
+            if(board[(3 * (row/3)) + (i/3)][ (3 * (col/3)) + (i % 3)] == validNum) return false;
+        }
+        
         return true;
     }
 }
