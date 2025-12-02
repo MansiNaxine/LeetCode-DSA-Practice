30aaceac8ab0e17331825dd37aab735f0ffa04a1
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-02 17:32:10 +0530

    Time: 10 ms (15.55%), Space: 65.3 MB (15.87%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,19 +1,14 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> partition(String s) {
 
         List<List<String>> answer = new ArrayList<>();
         List<String> currSeq = new ArrayList<>();
-
         helper(s, answer, currSeq, 0);
         return answer;
         
     }
 
-
     public void helper(String s, List<List<String>> answer, List<String> currSeq, int index) {
-
         //base case
         if(index == s.length()) {
             answer.add(new ArrayList<>(currSeq));
@@ -22,15 +17,20 @@ class Solution {
 
         //recursive case
         for(int i = index; i < s.length(); i++) {
-            StringBuilder sb = new StringBuilder(s.substring(index, i + 1));
-            String str = s.substring(index, i + 1);
-            if(str.equals(sb.reverse().toString()))  {
-                currSeq.add(str);
+            if(isPalindrome(s.substring(index, i + 1))) {
+                currSeq.add(s.substring(index, i + 1));
                 helper(s, answer, currSeq, i + 1);
                 currSeq.removeLast();
-            }   
-            
+            }
         }
+
     }
 
+    public boolean isPalindrome(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+        }
+
+        return true;
+    }
 }
