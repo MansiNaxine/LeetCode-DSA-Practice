0b31e2e9d61d464aac897d6e538b3f9a21b3d54e
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-15 23:08:52 +0530

    Time: 14 ms (14.19%), Space: 47 MB (15.95%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -5,18 +5,35 @@ class Solution {
         //Time Complexity :- O(N ^ 2)
         //Space Complexity :- O(1)
         int ans = 0;
-        for(int i = 0; i < nums.length; i++) {
-            boolean checkNum = false;
-            for(int j = 0; j < nums.length; j++) {
-                if(i != j && nums[i] == nums[j]) {
-                    checkNum = true;
-                    break;
-                }
-            }
+        // for(int i = 0; i < nums.length; i++) {
+        //     boolean checkNum = false;
+        //     for(int j = 0; j < nums.length; j++) {
+        //         if(i != j && nums[i] == nums[j]) {
+        //             checkNum = true;
+        //             break;
+        //         }
+        //     }
 
-            if(!checkNum) {
-                ans = nums[i];
+        //     if(!checkNum) {
+        //         ans = nums[i];
+        //     }
+        // }
+
+        //Better Approach
+        //Time Complexity :- O(N)
+        //Space Complexity :- O(N)
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
             }
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == 1) ans = entry.getKey();
         }
 
         //Optimal Solution
@@ -27,6 +44,8 @@ class Solution {
         //     ans  = ans ^ num;
         // }
 
-         return ans;
+        //  return ans;
+
+        return ans;
     }
 }
