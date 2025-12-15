7589e6241af0447c9171e8a68a16a630f2e2d948
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-15 23:03:09 +0530

    Time: 257 ms (7.41%), Space: 46.8 MB (43.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,32 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
+        //BruteForce Approach
+        //Time Complexity :- O(N ^ 2)
+        //Space Complexity :- O(1)
         int ans = 0;
-        for(int num :  nums) {
-            ans  = ans ^ num;
+        for(int i = 0; i < nums.length; i++) {
+            boolean checkNum = false;
+            for(int j = 0; j < nums.length; j++) {
+                if(i != j && nums[i] == nums[j]) {
+                    checkNum = true;
+                    break;
+                }
+            }
+
+            if(!checkNum) {
+                ans = nums[i];
+            }
         }
 
-        return ans;
+        //Optimal Solution
+        //Time Complexity :- O(N)
+        //Space Complexity :- O(1)
+        // int ans = 0;
+        // for(int num :  nums) {
+        //     ans  = ans ^ num;
+        // }
+
+         return ans;
     }
 }
