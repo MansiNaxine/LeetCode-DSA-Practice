84c53f60fcde6d449d1997002f2de19c66be0ea7
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-15 22:15:59 +0530

    Time: 2 ms (87.02%), Space: 47.9 MB (20.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,23 +5,37 @@ class Solution {
         //Time Complexity :- O(2N)
         //Space Complexity :- O(No. of non zero elements)
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
 
-        for(int num : nums) {
-            if(num != 0) list.add(num);
-        }
+        // for(int num : nums) {
+        //     if(num != 0) list.add(num);
+        // }
 
-        int index = 0;
-        for(int i = 0; i < n; i++) {
-            if(i > (list.size() -  1)) nums[i] = 0;
-            else {
-                nums[i] = list.get(index);
-                index++;
-            }
-        }
+        // int index = 0;
+        // for(int i = 0; i < n; i++) {
+        //     if(i > (list.size() -  1)) nums[i] = 0;
+        //     else {
+        //         nums[i] = list.get(index);
+        //         index++;
+        //     }
+        // }
 
         //Optimal Approach
         //Time Complexity :- O(2N)
         //Space Complexity :- O(No. of non zero elements)
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n) {
+            if(nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+
+        while(i < n) {
+            nums[i] = 0;
+            i++;
+        }
     }
 }
