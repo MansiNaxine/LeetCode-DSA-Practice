cbf31295a0194c2b47a6e15ef8781f88211214ae
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-16 21:03:31 +0530

    Time: 7 ms (45.87%), Space: 47.5 MB (11.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,58 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int answer[] = new int[2];
-        for(int i = 0; i < nums.length - 1; i++) {
-            int secondNum = target - nums[i];
-            for(int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == secondNum) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
+        int n = nums.length;
+        int nums1[] = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums1);
+        return calculateIndices(nums1, target, n, nums);
+    }
+
+    public int[] calculateIndices(int[] nums, int target, int n, int[] nums1) {
+        
+        int[] answer = new int[2];
+        int i = 0;
+        int j = n - 1;
+        int a = -1;
+        int b = -1;
+
+        while(i < j) {
+            int sum = nums[i] + nums[j] ;
+            if(sum == target) {
+                a = nums[i];
+                b = nums[j];
+                break;
+            } 
+            else if (sum < target) {
+                i++;
+            } 
+            else {
+                j--;
+            }
+        }
+
+        int startI = -1;
+        for(int k = 0; k < n; k++) {
+            if(a == b) {
+              if(nums1[k] == a) {
+                if(startI < 0) {
+                    startI = k;
+                    answer[0] = startI;
+                } else if (k != startI && startI != -1){
+                    answer[1] = k;
                 }
+              }
+            } 
+            else if(nums1[k] == a) {
+                answer[0] = k;
+            }
+            else if (nums1[k] == b) {
+                answer[1] = k;
             }
         }
 
         return answer;
+
     }
 }
