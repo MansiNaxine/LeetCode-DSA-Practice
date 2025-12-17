e858e602c46ce66306c467dccf45e2fc861c567f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-17 13:37:17 +0530

    Time: 3 ms (99.91%), Space: 175.6 MB (26.73%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,6 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        
+        //Time Complexity :- O(2N)
+        //Space Complexity :- O(n)
+        //return bruteForaceAprroach(nums);
+
+        //Time Complexity :- O(N)
+        //Space Complexity :- O(N)
+        return optimalApproach(nums);
+    }
+
+    public int[] optimalApproach(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+        int pos = 0;
+        int neg = 1;
+
+        for(int num : nums) {
+            if (num > 0) {
+                answer[pos] = num;
+                pos += 2;
+            }
+            else {
+                answer[neg] = num;
+                neg += 2;
+            }
+        }
+
+        return answer;
+    }
+
+    public int[] bruteForaceAprroach(int[] nums){
         int n = nums.length;
         int pos[] = new int[n/2];
         int neg[] = new int[n/2];
