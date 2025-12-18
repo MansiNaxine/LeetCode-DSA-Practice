2f21ed501f5431c4a2714afceeba6a4bc24dc1c2
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-18 22:22:57 +0530

    Time: 0 ms (100%), Space: 44.7 MB (17.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,33 +2,49 @@ class Solution {
     public void nextPermutation(int[] nums) {
 
         int n = nums.length;
-        int i = n - 2;
+        int j = n - 2;
+        // int small = -1;
+        // int jIndex = -1;
 
-        while(i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
+        //First fine the element which is less than the next element from end
+        while(j >= 0) {
+            if(nums[j] < nums[j + 1]) {
+                // small = nums[j];
+                // jIndex = j;
+                break;
+            }
+            j -= 1;
         }
 
-        if( i >= 0) {
-            int j = n - 1;
-            while(j > 0 && nums[j] <= nums[i]) {
-                j--;
-            }
+        int i = j + 1;
+        // int nextGreater = -1;
 
-            int temp = nums[i];
-            nums[i] = nums[j];
+        //find the next greater element than small
+
+        while(i < n && j >= 0 && nums[i] > nums[j]) {
+            i += 1;
+        }
+
+        //swap both small and nextGreater element
+        if(i > 0) {
+            int temp = nums[i - 1];
+            nums[i - 1] = nums[j];
             nums[j] = temp;
         }
 
-        int k = i + 1;
-        int m = n - 1;
-
-        while(k < m) {
-            int temp = nums[k];
-            nums[k] = nums[m];
-            nums[m] = temp;
-            k++;
-            m--;
+        if((n - j) > 1) {
+            swap(nums, j + 1, n - 1);
         }
-        
+    }
+
+    public void swap(int[] nums, int start, int end) {
+
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
