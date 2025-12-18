3b0dff878e6a9ff3a983e317c12c308d9c981a36
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-18 22:26:45 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
 
+        //Total time complexity will take :- O(2n)
+        //Space Complexity will be :- O(1)
         int n = nums.length;
         int j = n - 2;
-        // int small = -1;
-        // int jIndex = -1;
 
         //First fine the element which is less than the next element from end
+        //Time Complexity :- O(n)
         while(j >= 0) {
             if(nums[j] < nums[j + 1]) {
-                // small = nums[j];
-                // jIndex = j;
                 break;
             }
             j -= 1;
         }
 
         int i = j + 1;
-        // int nextGreater = -1;
 
         //find the next greater element than small
-
+        //Time Complexity :- by combining both above step and below step will tale O(n)
         while(i < n && j >= 0 && nums[i] > nums[j]) {
             i += 1;
         }
 
         //swap both small and nextGreater element
+        //Time Complexity :- O(1)
         if(i > 0) {
             int temp = nums[i - 1];
             nums[i - 1] = nums[j];
             nums[j] = temp;
         }
 
+        //Time Complexity :- O(n)
         if((n - j) > 1) {
             swap(nums, j + 1, n - 1);
         }
