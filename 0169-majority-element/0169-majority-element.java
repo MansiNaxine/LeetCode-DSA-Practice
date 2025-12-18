1af25a157e183215f4826d227089f4169a106f37
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-18 16:34:17 +0530

    Time: 2 ms (74.85%), Space: 56.1 MB (8.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        int count = 0;
-        int answer = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(count == 0) {
-                answer = nums[i];
+        //BruteForceApproach 
+        //Time Complexity :-  O(n^2)
+        //Space Complexity :-  O(1)
+
+        int count = 1;
+        int element = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if (nums[i] == element) {
                 count++;
-            } else {
-                if(nums[i] == answer) {
-                    count++;
-                } else {
-                    count--;
-                }
+            }
+            else if(count == 0) {
+                element = nums[i];
+                count = 1;
+            }
+            else {
+                count--;
             }
         }
 
-        return answer;
+        int occurence = nums.length/2;
+        count = 0;
+        for(int num :  nums) {
+            if(element == num) count++;
+        }
+
+        if(count > occurence) return element;
+
+        return element;
         
     }
 }
