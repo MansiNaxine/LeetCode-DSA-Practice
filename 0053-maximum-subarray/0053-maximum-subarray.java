9d8328538a0bdc20f55c669ddd496f00679c2e6d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-18 14:10:23 +0530

    Time: 1 ms (99.76%), Space: 77.3 MB (32.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,18 +1,29 @@
 class Solution {
     public int maxSubArray(int[] nums) {
 
-        int n = nums.length;
-        int max = Integer.MIN_VALUE;
+        //BruteForce Approach
+        //Time Complexity :- O(n^3)
+        //Space Complexity :- O(1)
+
+        //Better Solution
+        //Time Complexity :- O(n^2)
+        //Space Complexity :- O(1)
+
+        //Optimal Solution
+        //Kadane's Algorithm
         int sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            max = Math.max(max, sum) ;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int num : nums) {
+            
+            sum += num;
+            maxSum = Math.max(sum, maxSum);
+
             if(sum < 0) {
                 sum = 0;
             }
         }
 
-        return max;
-        
+        return maxSum;
     }
 }
