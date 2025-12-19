649cf661ce9c51bfd5eb3c238d60459e878cb7d3
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-19 13:33:48 +0530

    Time: 1 ms (61.75%), Space: 47.7 MB (6.62%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int n = matrix.length;
+        int[][] answerMatrix = new int[n][matrix[0].length];
+        
+        
+        //Check for zero value
+        //And call function to mark complete corresponding row and column as -1
+        for(int i = 0; i < n; i++) {
+                int row = -1;
+                int col = -1;
+            for(int j = 0 ; j < matrix[i].length; j++) {
+                if(matrix[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    changeMatrix(answerMatrix, row, col);
+                }
+            }
+        }
+
+        //change all marked element as -1 to 0
+        for(int i = 0; i < n; i++) {
+            for(int j = 0 ; j < answerMatrix[i].length; j++) {
+                if(answerMatrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                } 
+            }
+        }
+
+        
+    }
+
+    public void changeMatrix(int[][] matrix, int row, int col) {
+
+        for(int j = 0; j < matrix[0].length; j++) {
+             matrix[row][j] = -1;
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+             matrix[i][col] = -1;
+        }
+    }
+}
