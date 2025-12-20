c1733b132bdddf3d2753911f6e477716ee75dc1c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-20 15:15:11 +0530

    Time: 11 ms (16.04%), Space: 49.6 MB (7.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,81 +5,34 @@ class Solution {
 
         return bruteForceApproach(intervals);
 
-        // return optimalApproach(intervals);
-
-    }
-
-    public int[][] optimalApproach(int[][] intervals) {
-
-        int n = intervals.length;
-        //Sort the array
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int[][] answer = new int[n][2];
-        answer[0][0] = intervals[0][0];
-        answer[0][1] = intervals[0][1];
-        int index = 1;
-
-        for(int i = 1; i < n; i++) {
-            if(answer.length != 0) {
-                int secondNum = answer[answer.length - 1][1];
-                int firstCurrentINum = answer[i][0];
-                int secondCurrentINum = answer[i][1];
-                if( firstCurrentINum <= secondNum && secondCurrentINum > secondNum) {
-                    int actualNum = intervals[i][1];
-                    answer[answer.length - 1][1] = actualNum;
-                }
-                else if (secondCurrentINum <= secondNum) {
-                    continue;
-                }
-                else {
-                    answer[index][0] = intervals[i][0];
-                    answer[index][1] = intervals[i][1];
-
-                    index++;
-                }
-            }
-        }
-
-        return answer;
     }
 
     public int[][] bruteForceApproach(int[][] intervals) {
         int n = intervals.length;
         int m = 2;
 
-        List<List<Integer>> intervalsList = new ArrayList<>();
-
-        //first copy all elements for intervals array to intervals to list
-        for(int i = 0; i < n; i++) {
-            List<Integer> currList = new ArrayList<>();
-            for(int j = 0; j < m; j++) {
-                    currList.add(intervals[i][j]);
-            }
-            intervalsList.add(new ArrayList<>(currList));
-        }
-
-        
-
-        Collections.sort(intervalsList, (a, b) -> Integer.compare(a.get(0), b.get(0)));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<List<Integer>> answerList = new ArrayList<>();
+        List<Integer> currList1 = new ArrayList<>();
+        currList1.add(intervals[0][0]);
+        currList1.add(intervals[0][1]);
 
-        answerList.add(new ArrayList<>(intervalsList.get(0)));
+        answerList.add(new ArrayList<>(currList1));
 
         for(int i = 1; i < n; i++) {
             if(!answerList.isEmpty()) {
                 int secondNum = answerList.getLast().get(1);
-                int firstCurrentINum = intervalsList.get(i).get(0);
-                int secondCurrentINum = intervalsList.get(i).get(1);
+                int firstCurrentINum = intervals[i][0];
+                int secondCurrentINum = intervals[i][1];
                 if( firstCurrentINum <= secondNum ) {
-                    int actualNum = intervalsList.get(i).get(1);
+                    int actualNum = intervals[i][1];
                     answerList.getLast().set(1, Math.max(actualNum, secondNum));
                 }
                 else {
                     List<Integer> currList = new ArrayList<>();
-                    currList.add(intervalsList.get(i).get(0));
-                    currList.add(intervalsList.get(i).get(1));
+                    currList.add(intervals[i][0]);
+                    currList.add(intervals[i][1]);
 
                     answerList.add(new ArrayList<>(currList));
                 }
