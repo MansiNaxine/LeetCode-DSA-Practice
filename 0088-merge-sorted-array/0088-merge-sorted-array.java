970f127ea9ab5b9826881d5c4f0228b278429c9c
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-23 16:45:19 +0530

    Time: 4 ms (12.64%), Space: 44.1 MB (9.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -2,6 +2,51 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
         //Optimal Solution
+        //this is only to reduce space complexity
+        firstOptimalSolution(nums1, m, nums2, n);
+
+        //optimalSolutionUsingShellSort()
+        optimalSolutionUsingShellSort(nums1, m, nums2, n);
+        return;
+        
+    }
+
+    public void optimalSolutionUsingShellSort(int[] nums1, int m, int[] nums2, int n) {
+
+        int len = m + n;
+        int gap = (len/2) + (len%2);
+
+        while(gap > 0) {
+            int left = 0;
+            int right = gap + left;
+            while(left < len && right < n) {
+                //1st case will include arr1 and arr2
+                if (left < m && right < n) {
+                    swapElements(nums1, nums2, left, right);
+                }
+                else if (left >= m && right < n) { // both will present in 2nd array
+                    swapElements(nums2, nums2, left, right);
+                }
+                else { //both will present in 1st array
+                    swapElements(nums1, nums1, left, right);
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) break;
+            gap = (gap/2) + (gap%2);
+        }
+    }
+
+    public void swapElements(int[] nums1, int[] nums2, int left, int right) {
+        if(nums1[left] > nums2[right]) {
+            int temp = nums1[left];
+            nums1[left] = nums2[right];
+            nums2[right] = temp;
+        }
+    }
+
+    public void firstOptimalSolution(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = 0;
 
@@ -27,6 +72,5 @@ class Solution {
             nums1[index] = nums2[k];
             index++;
         }
-        
     }
 }
