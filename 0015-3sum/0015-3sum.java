3afc113c3fc8d82933e8d82428798f955b462ba4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2025-12-23 22:49:42 +0530

    Time: 33 ms (63.21%), Space: 59.2 MB (37.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,51 +1,56 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        //first I will sort the array
+
         Arrays.sort(nums);
-
-        //first form the answer list
         List<List<Integer>> answer = new ArrayList<>();
-        int i = 0;
+        int i = 0; 
         int n = nums.length;
-
+        
         while(i < n) {
-            int currentI = nums[i];
-            int findSecondNum = -nums[i];
+            int currI = nums[i];
+            int target = -nums[i];
             int j = i + 1;
             int k = n - 1;
 
-            //binary search to find the second number which is negative of current I
             while(j < k) {
                 int sum = nums[j] + nums[k];
-                if(sum == findSecondNum) {
-                    List<Integer> currentList = new ArrayList<>();
-                    //adding all nums of I, J, K
-                    currentList.add(nums[i]);
-                    currentList.add(nums[j]);
-                    currentList.add(nums[k]);
+                if(sum == target) {
+                    List<Integer> currSeq = new ArrayList<>();
+                    currSeq.add(nums[i]);
+                    currSeq.add(nums[j]);
+                    currSeq.add(nums[k]);
 
-                    while(j < n && nums[j] == currentList.get(1)) {
-                        j++;
+                    answer.add(new ArrayList<>(currSeq));
+                    int currJ = nums[j];
+                    int newJ = j + 1;
+                    while(newJ < k && nums[newJ] == currJ) {
+                        newJ++;
                     }
-
-                    while(k >=0 && nums[k] == currentList.get(2)) {
-                        k--;
+                    j = newJ;
+                    
+                    int currK = nums[k];
+                    int newK = k - 1;
+                    while(newK > j && nums[newK] == currK) {
+                        newK--;
                     }
-
-                    answer.add(new ArrayList<>(currentList));
-                } else if(sum < findSecondNum) {
+                    k = newK;
+                }
+                else if (sum < target) {
                     j++;
-                } else {
+                }
+                else {
                     k--;
                 }
             }
 
-            while(i < n && nums[i] == currentI) {
-                i++;
+            int newIndex = i + 1;
+            while(newIndex < n && nums[newIndex] == currI) {
+                newIndex++;
             }
+            i = newIndex;
         }
 
         return answer;
+        
     }
 }
