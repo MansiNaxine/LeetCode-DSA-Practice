74b2b6bb3d5c0cd342b75d695239e6ad81c8f6fa
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-01-26 16:14:44 +0530

    Time: 0 ms (100%), Space: 43.7 MB (65.97%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,26 +1,38 @@
 class Solution {
     public int findMin(int[] nums) {
-
+        
         int n = nums.length;
+        //base cases
+        if(n == 1) return nums[0];
+        if(n == 2 && nums[0] < nums[1]) return nums[0];
+        if(n == 2 && nums[0] > nums[1]) return nums[1];
+        if(nums[0] < nums[n - 1]) return nums[0];
+
+        //Binary Search
         int start = 0;
         int end = n - 1;
 
-
         while(start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[(mid - 1 + n) % n] > nums[mid] && nums[mid] < nums[(mid + 1) % n]) {
+
+            int mid = (start + end)/2;
+
+            if(mid == 0 && nums[mid] < nums[mid + 1]) {
                 return nums[mid];
-            } else if (nums[start] <=  nums[end]) {
-                return nums[start];
-            } else if(nums[start] <= nums[mid]) {
+            }
+            else if (mid == n - 1 && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            else if ((mid != 0 && nums[mid] < nums[mid - 1]) && (mid != n - 1 && nums[mid] < nums[mid + 1])) {
+                return nums[mid];
+            }
+            else if (nums[start] <= nums[mid] && nums[mid] > nums[end]){
                 start = mid + 1;
-            } else {
+            }
+            else {
                 end = mid - 1;
-            } 
+            }
         }
 
-        
-        
         return -1;
     }
 }
