918430d347ee208281068e8be16bb0483d9ba25d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-01-26 17:37:31 +0530

    Time: 0 ms (100%), Space: 44.5 MB (11.59%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,35 +1,33 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        
-        int n = nums.length;
-        int start = 0;
-        int end = n - 1;
 
-        if(n == 1) return start;
+        int n = nums.length;
+
+        //Boundary cases
+        int boundryValue = Integer.MIN_VALUE;
+        if(n == 1) return 0;
+        if(nums[0] > boundryValue && nums[0] > nums[1]) return 0;
+        if(nums[n - 1] > boundryValue && nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int start = 1;
+        int end = n - 2;
 
         while(start <= end) {
-            int mid = (start + end) / 2;
-            if((mid - 1) == -1) {
-                if(nums[mid] > nums[mid + 1]) {
-                    return mid;
-                } else {
-                    start = mid + 1;
-                }
-            } else if ((mid + 1) == n) {
-                if(nums[mid] > nums[mid - 1]) {
-                    return mid;
-                } else {
-                    end = mid - 1;
-                }
-            } else if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+
+            int mid = (start + end)/2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            } else if (nums[mid] < nums[mid + 1]) {
+            }
+            else if (nums[mid] < nums[mid + 1]) {
                 start = mid + 1;
-            } else {
+            }
+            else {
                 end = mid - 1;
             }
         }
 
-        return 0;
+        return -1;
+        
     }
 }
