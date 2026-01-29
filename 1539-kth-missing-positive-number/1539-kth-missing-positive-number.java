1750efaadc3d4a95d904e2e020dd888cdec55ba4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-01-29 12:47:30 +0530

    Time: 0 ms (100%), Space: 44.6 MB (67.97%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        
+        return bruteForceApproach(arr, k);
+        
+    }
+
+    public int bruteForceApproach(int[] arr, int k) {
+
+        for(int num : arr) {
+            if(num <= k) k++;
+            else break;
+        }
+
+        return k;
+    }
+}
