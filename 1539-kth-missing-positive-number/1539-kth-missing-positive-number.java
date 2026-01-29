012d9b1240ec6293c056e00768da0c5a78d65923
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-01-29 16:16:41 +0530

    Time: 0 ms (100%), Space: 44.8 MB (32.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,8 +1,32 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         
-        return bruteForceApproach(arr, k);
+        // return bruteForceApproach(arr, k);
+
+        return optimalApproachUsingBS(arr, k);
         
+    }
+
+    public int optimalApproachUsingBS(int[] arr, int k) {
+
+        int n = arr.length;
+        int start = 0;
+        int end = n - 1;
+
+        while(start <= end) {
+
+            int mid = (start + end)/2;
+            int missingNum = arr[mid] - (mid + 1);
+
+            if(missingNum < k) {
+                start = mid + 1;
+            }
+            else {
+                end= mid - 1;
+            }
+        }
+
+        return end + 1 + k;
     }
 
     public int bruteForceApproach(int[] arr, int k) {
