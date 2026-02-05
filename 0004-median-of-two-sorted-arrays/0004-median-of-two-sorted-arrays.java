46ec6972fca0762649fc25c9cd35c326632c1102
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-05 13:18:57 +0530

    Time: 1 ms (99.46%), Space: 48.8 MB (68.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,6 +1,56 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
+        // double answer = bruteForceApproach(nums1, nums2);
+        double answer = optimalApproach(nums1, nums2);
+
+        return answer;
+    }
+
+    public double optimalApproach(int[] nums1, int[] nums2) {
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1)/2;
+        if(n1 > n2) return optimalApproach(nums2, nums1);
+        int low = 0;
+        int high = n1;
+
+        while(low <= high) {
+
+            int m1 = (low + high)/2;
+            int m2 = left - m1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if(m1 < n1) r1 = nums1[m1];
+            if(m2 < n2) r2 = nums2[m2];
+            if(m1 > 0) l1 = nums1[m1 - 1];
+            if(m2 > 0) l2 = nums2[m2 - 1];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if ((n % 2) == 1) {
+                    return (double) Math.max(l1, l2);
+                }
+                else {
+                    return (double) ((Math.max(l1, l2)) + (Math.min(r1, r2)))/2;
+                }
+            }
+            else if (l1 > r2) {
+                high = m1 - 1;
+            }
+            else {
+                low = m1 + 1;
+            }
+        }
+
+        return 0;
+
+    }
+
+    public double bruteForceApproach(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
         int n = n1 + n2;
