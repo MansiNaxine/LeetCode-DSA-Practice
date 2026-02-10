41d3f4166dc2517e6d142d56b420f7a7ee552721
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-10 09:44:08 +0530

    Time: 82 ms (5.06%), Space: 47.9 MB (7.21%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int n = s.length();
+        int r = 0;
+        int l = 0;
+        int maxLen = 1;
+        int diff = 0;
+        int maxF = Integer.MIN_VALUE;
+        int len = 0;
+        
+        //map will be of 26 size as uestion only contains 26 UPPERCASE letters
+        Map<Character, Integer> map = new HashMap<>();
+        
+        while(r < n) {
+            if (!map.containsKey(s.charAt(r))) {
+                map.put(s.charAt(r), 1);
+            }
+            else {
+                map.put(s.charAt(r), map.get(s.charAt(r)) + 1);
+            }
+
+            len = (r - l + 1);
+            maxF = calculateMaxF(map);
+            diff = len - maxF;
+
+            if(diff > k && l <= r) {
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                if(map.get(s.charAt(l)) == 0) {
+                    map.remove(s.charAt(l));
+                    len = (r - l + 1);
+                    maxF = calculateMaxF(map);
+                    diff = len - maxF;
+                }
+                l++;
+            }
+
+            if(diff <= k) {
+                maxLen = Math.max(maxLen, len);
+            }
+
+            r++;
+        }
+
+
+        return maxLen;
+
+        
+    }
+
+    public int calculateMaxF(Map<Character, Integer> map) {
+        int maxF = 0;
+
+        for(Map.Entry<Character, Integer> mp : map.entrySet()) {
+            maxF = Math.max(maxF, mp.getValue());
+        }
+
+        return maxF;
+    }
+}
