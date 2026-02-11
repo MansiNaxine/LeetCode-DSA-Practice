9067455e71c44a53db0dcd4de5958749f160d4dd
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-11 09:42:27 +0530

    Time: 2 ms (99.6%), Space: 62.2 MB (13.75%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -12,20 +12,11 @@ class Solution {
             leftSum += cardPoints[i];
             lIn += 1;
         }
-
+        
+        //Boundry Case
         if(k == n) return leftSum;
 
         maxScore = leftSum;
-
-
-        //Now travel from K to N-1 elements by using sliding window approach and calculate the sum accordingly
-        // for(int i = n - 1; i > k ; i--) {
-        //     rightSum += cardPoints[i];
-        //     leftSum -=  cardPoints[lIn];
-            
-        //     // lIn -= 1;
-        //     maxScore = Math.max(maxScore, (rightSum + leftSum));
-        // }
         int rIn = n - 1;
         for(int i = lIn ; i >= 0; i--) {
             leftSum = leftSum - cardPoints[i];
