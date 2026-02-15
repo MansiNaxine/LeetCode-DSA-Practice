f9f7b468995a5e730421a748199b6dd479ac6668
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 20:31:49 +0530

    Time: 10 ms (85.46%), Space: 50.4 MB (35.54%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -12,14 +12,14 @@ class Solution {
 
         int start = max;
         int end = sum;
-        int ans = -1;
+        int ans = max;
 
         while(start <= end) {
 
             int mid = (start + end)/2;
-            int possibleDays = getNoOfDays(weights, mid);
+            int possibleDays = getPossibleDays(weights, mid);
 
-            if(possibleDays <= days) {
+            if (possibleDays <= days) {
                 ans = mid;
                 end = mid - 1;
             }
@@ -27,33 +27,27 @@ class Solution {
                 start = mid + 1;
             }
         }
-        
+
         return ans;
+        
     }
 
-    public int getNoOfDays(int[] weights, int mid) {
+    public int getPossibleDays(int[] weights, int mid) {
 
         int sum = 0;
         int days = 0;
-
         for(int weight : weights) {
             if((sum + weight) <= mid) {
                 sum += weight;
-            } else {
+            }
+            else {
                 days += 1;
                 sum = weight;
-                if(weight > mid) {
-                    days += (weight/mid);
-                    if((weight%mid) > 0 && (weight%mid) <= mid) days += 1;
-                }
             }
+
+
         }
 
-        if(sum != 0 && sum <= mid) return days + 1;
-        else {
-            days += (sum/mid);
-            if((sum%mid) > 0 && (sum%mid) <= mid) days += 1;
-            return days;
-        }
+        return days + 1;
     }
 }
