4afa96d08e5bd121e74a012fa69d94dd3df5d5c2
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 15:51:37 +0530

    Time: 0 ms (100%), Space: 44.1 MB (25.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-
+        
         int n = nums.length;
         int start = 0;
         int end = n - 1;
-        int ans = -1;
 
         while(start <= end) {
-            
-            int mid = (start + end) / 2;
 
-            if(nums[mid] == target) {
-                ans = mid;
-                return ans;
+            int mid = (start + end)/2;
+
+            if (nums[mid] == target) {
+                return mid;
             }
-            else if (nums[start] <= nums[mid] ) {
-
-                if (target < nums[mid] && target >= nums[start]) {
+            else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]) {
                     end = mid - 1;
                 }
                 else {
                     start = mid + 1;
                 }
             }
-            else {
-
-                if(target > nums[mid] && target <= nums[end]) {
+            else if(nums[mid] <= nums[end]){
+                 if (target >= nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 }
                 else {
@@ -34,7 +30,6 @@ class Solution {
             }
         }
 
-        return ans;
-        
+        return -1;
     }
 }
