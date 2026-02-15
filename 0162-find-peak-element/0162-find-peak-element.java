168a6a9038da12f1e9def29d69fd86ced0be55fe
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 17:51:59 +0530

    Time: 0 ms (100%), Space: 44.2 MB (74.29%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,22 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        //[1,2,3,1]
+        //[1,2,1,3,5,6,4]
 
         int n = nums.length;
-
-        //Boundary cases
-        int boundryValue = Integer.MIN_VALUE;
+        int start = 0;
+        int end = n - 1;
+        //edge condition
         if(n == 1) return 0;
-        if(nums[0] > boundryValue && nums[0] > nums[1]) return 0;
-        if(nums[n - 1] > boundryValue && nums[n - 1] > nums[n - 2]) return n - 1;
-
-        int start = 1;
-        int end = n - 2;
 
         while(start <= end) {
 
             int mid = (start + end)/2;
 
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+            if((mid > 0 && nums[mid] > nums[mid - 1]) && (mid < n - 1 && nums[mid] > nums[mid + 1])) {
+                return mid;
+            }
+            else if ((mid == 0 && nums[mid] > nums[mid + 1]) || (mid == n - 1 && nums[mid] > nums[mid - 1])) {
                 return mid;
             }
             else if (nums[mid] < nums[mid + 1]) {
@@ -28,6 +28,6 @@ class Solution {
         }
 
         return -1;
-        
+
     }
 }
