d20ff6b4b22fdf1a424156fc016015ce9a931c12
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 17:34:33 +0530

    Time: 0 ms (100%), Space: 52.8 MB (77.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,42 +2,28 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
 
         int n = nums.length;
-
-        //base cases
-        if(n == 1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
-
         int start = 0;
         int end = n - 1;
+        if(n == 1) return nums[0];
 
         while(start <= end) {
 
-            int mid = (start + end) / 2;
+            int mid = (start + end)/2;
 
-            if ((mid != 0 && nums[mid] != nums[mid - 1]) && ( mid != n - 1 && nums[mid] != nums[mid + 1])) {
+            if((mid > 0 && nums[mid] != nums[mid - 1]) && (mid < n - 1 && nums[mid] != nums[mid + 1])) {
                 return nums[mid];
             }
-            else if (nums[mid] == nums[mid - 1]) {
-                if((mid - 1)%2 == 0) {
-                    start = mid + 1;
-                }
-                else {
-                    end = mid - 1;
-                }
+            else if ((mid == 0 && nums[mid] != nums[mid + 1]) || (mid == n - 1 && nums[mid] != nums[mid - 1])) {
+                return nums[mid];
             }
-            else if (nums[mid] == nums[mid + 1]){
-                if((mid + 1)%2 == 0) {
-                    end = mid - 1;
-                }
-                else {
-                    start = mid + 1;
-                }
-                
+            else if ((mid%2 == 0 && nums[mid] == nums[mid + 1]) || (mid%2 == 1 && nums[mid] == nums[mid - 1])) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
             }
         }
 
-        return nums[n - 1];
-        
+        return nums[0];
     }
 }
