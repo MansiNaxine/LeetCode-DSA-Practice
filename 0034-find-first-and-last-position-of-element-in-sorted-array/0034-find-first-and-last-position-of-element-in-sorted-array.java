744db404789ce9ead16ae2225c5d81838874f736
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 14:58:14 +0530

    Time: 0 ms (100%), Space: 48.6 MB (11.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,55 +2,18 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
 
         int n = nums.length;
-        int firstOccu = findFirstOccurence(nums, target);
-        int secondOccu = -1;
-        if(firstOccu != -1) {
-            secondOccu = findSecondOccurence(nums, target);
-        }
-        else {
-            return new int[]{-1, -1};
-        }
-
-        return new int[]{firstOccu, secondOccu};
-    }
-
-    public int findFirstOccurence(int[] nums, int target) {
-
-        int ans = -1;
-        int n = nums.length;
         int start = 0;
         int end = n - 1;
+        int firstOccur = -1;
+        int lastOccur = -1;
 
+        //to find first occurrence
         while(start <= end) {
 
-            int mid = (start + end) /2;
-
-            if (nums[mid] >= target) {
-                ans = mid;
-                end = mid - 1;
-            }
-            else {
-                start = mid + 1;
-            }
-        }
-
-        if(ans >= 0 && nums[ans] != target) return -1;
-        return ans;
-    }
-
-    public int findSecondOccurence(int[] nums, int target) {
-
-        int ans = -1;
-        int n = nums.length;
-        int start = 0;
-        int end = n - 1;
-
-        while(start <= end) {
-
-            int mid = (start + end) /2;
+            int mid = (start + end)/2;
 
             if (nums[mid] <= target) {
-                ans = mid;
+                lastOccur = mid;
                 start = mid + 1;
             }
             else {
@@ -58,7 +21,27 @@ class Solution {
             }
         }
 
-        if(ans >= 0 && nums[ans] != target) return -1;
-        return ans;
+        //to find last occurrence
+        start = 0;
+        end = n - 1;
+
+        while(start <= end) {
+
+            int mid = (start + end)/2;
+
+            if (nums[mid] >= target) {
+                firstOccur = mid;
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        firstOccur = (firstOccur != -1 && nums[firstOccur] == target) ? firstOccur : -1;
+        lastOccur = (lastOccur != -1 && nums[lastOccur] == target) ? lastOccur : -1;
+
+        return new int[]{firstOccur, lastOccur};
+
     }
 }
