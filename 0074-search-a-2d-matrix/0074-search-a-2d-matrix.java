9b0c7c7f71974a6be731192049fa3b2f9c783401
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-17 08:45:25 +0530

    Time: 0 ms (100%), Space: 44.2 MB (7.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,20 +3,23 @@ class Solution {
 
         int n = matrix.length;
         int m = matrix[0].length;
-        //Convert 2D matrix into 1D, by keeping below low and high at specified indexes
-        int low = 0;
-        int high = (n * m) - 1;
+        int start = 0;
+        int end = (n * m) - 1;
 
-        while(low <= high) {
-            int mid = (low + high)/2;
-            //We have to get the row and column using mid
+        while(start <= end) {
+            int mid = (start + end)/2;
             int row = mid/m;
-            int col = mid%m;
+            int column = mid%m;
 
-            if(matrix[row][col] == target) return true;
-            else if (matrix[row][col] < target) low = mid + 1;
-            else high = mid - 1;
-
+            if (matrix[row][column] == target) {
+                return true;
+            }
+            else if (matrix[row][column] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
         }
 
         return false;
