fd9d952bae32bba5aaa3e9657804ee8ffd42c00d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-18 12:37:53 +0530

    Time: 3 ms (52.64%), Space: 51 MB (64.81%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,35 +1,33 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-
-        int n = nums.length;
-        int ans1 = findForGoal(nums, goal, n);
-        int ans2 = 0;
-        if(goal - 1 < 0) ans2 = 0;
-        else ans2 = findForGoal(nums, goal - 1, n);
-
-        return ans1 -  ans2;
         
+        int ans1 = numSubarraysWithSumAndLess(nums, goal);
+        int ans2 = 0;
+        if(goal - 1 < 0) return ans1;
+        else ans2 = numSubarraysWithSumAndLess(nums, goal - 1);
+
+        return ans1- ans2;
     }
 
-    public int findForGoal(int[] nums, int goal, int n) {
+    public int numSubarraysWithSumAndLess(int[] nums, int goal) {
 
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
         int sum = 0;
-        int r = 0;
-        int l = 0;
         int cnt = 0;
 
-        while(r < n) {
-            sum += nums[r];
+        while(right < n) {
+
+            sum += nums[right];
 
             while(sum > goal) {
-                sum -= nums[l];
-                l++;
+                sum -= nums[left];
+                left += 1;
             }
-            
-            if(sum <= goal) {
-                cnt += (r - l + 1);
-            }
-            r++;
+
+            if(sum <= goal) cnt += (right - left + 1);
+            right++;
         }
 
         return cnt;
