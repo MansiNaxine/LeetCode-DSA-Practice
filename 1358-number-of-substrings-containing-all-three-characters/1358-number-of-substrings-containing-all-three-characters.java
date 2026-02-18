b22b0b3e0e5ad0587f5ea059b830a2f36f83b08e
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-18 13:27:09 +0530

    Time: 32 ms (18.24%), Space: 46.9 MB (16.35%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,23 +1,23 @@
 class Solution {
     public int numberOfSubstrings(String s) {
 
-        int ans1 = getNumberOfStrings(s);
-        return ans1;
-        
-    }
-
-    public int getNumberOfStrings(String s) {
         int n = s.length();
+        int sum = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int i = 0;
         int cnt = 0;
-        int freq[] = {-1, -1, -1};
 
-        for(int i = 0 ; i < n; i++) {
-            freq[s.charAt(i) - 'a'] = i;
-            if(freq[0] != -1 && freq[1] != -1 && freq[2] != -1) {
-                cnt += (1 + Math.min(freq[0], Math.min(freq[1], freq[2]))) ; 
+        while(i < n) {
+            //inserting frequencies of characters
+            map.put(s.charAt(i), i);
+
+            if(map.size() == 3) {
+                cnt = cnt + Math.min(map.get('a'), Math.min(map.get('b'), map.get('c'))) + 1;
             }
+            i++;
         }
 
         return cnt;
+        
     }
 }
