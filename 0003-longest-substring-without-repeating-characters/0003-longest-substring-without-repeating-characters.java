72fe0cc0dde7722e4609d63ef268cd7a6f3fda17
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-18 10:26:36 +0530

    Time: 8 ms (18.87%), Space: 46.8 MB (19.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,61 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
 
-        //BruteForce Approach
-        //Time Complexity is taking O(N^2)
-        // int answer = bruteForceApproach(s);
-
-        //Optimal Solution Using Sliding window and two pointer concepts
-        int answer = optimalApproach(s);
-        return answer;
-        
-    }
-
-    public int optimalApproach(String s) {
-
         int n = s.length();
-        int maxLen = 0;
-        //Take map to store character and index position
         Map<Character, Integer> map = new HashMap<>();
-
-        int left = 0;
-        int right = 0;
-        while(right < n) {
-            
-            if(map.containsKey(s.charAt(right)) && left <= map.get(s.charAt(right))) {
-              left = map.get(s.charAt(right)) + 1;
-            }
-            map.put(s.charAt(right), right);
-            maxLen = Math.max(maxLen, (right - left + 1));
-            right += 1;
-
-        }
-
-        return maxLen;
-
-    }
-
-    public int bruteForceApproach(String s) {
-
-        int n = s.length();
+        int i = 0;
+        int j = 0;
         int maxLength = 0;
-        
 
-        for(int i = 0; i < n; i++) {
-            String str = "";
-            int freq[] = new int[256];
-            for(int j = i; j < n; j++) {
-                if(freq[(int) s.charAt(j)] == 0) {
-                    freq[(int) s.charAt(j)]++;
-                    str += s.charAt(j);
+        while(j < n) {
+            if(!map.containsKey(s.charAt(j))) {
+                int len = j - i + 1;
+                if(len > maxLength) {
+                    maxLength = len;
                 }
-                else {
-                    break;
-                }
+                map.put(s.charAt(j), j);
             }
-            maxLength = Math.max(maxLength, str.length());
+            else {
+                int newIndex = map.get(s.charAt(j));
+                while(i <= newIndex) {
+                    map.remove(s.charAt(i));
+                    i += 1;
+                }
+                map.put(s.charAt(j), j);
+            }
+            j++;
         }
 
         return maxLength;
+        
     }
 }
