9f32a10d240b9b4883a520ccbaea869e6287525b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-19 10:03:34 +0530

    Time: 50 ms (43.05%), Space: 50.6 MB (12.18%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+
+        int ans1 = calculateNumberOfSubArrays(nums, k);
+        int ans2 = 0;
+        if(k - 1 < 0) return ans1;
+        else ans2 = calculateNumberOfSubArrays(nums, k - 1);
+
+        return ans1 - ans2;
+    }
+
+    public int calculateNumberOfSubArrays(int[] nums, int k) {
+
+        int n = nums.length;
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while(j < n) {
+
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            while(map.size() > k) {
+                map.put(nums[i] , map.get(nums[i]) - 1);
+                if(map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+
+            if(map.size() <= k) {
+                cnt += (j - i + 1);
+            }
+            j++;
+        }
+
+        return cnt;
+    }
+}
