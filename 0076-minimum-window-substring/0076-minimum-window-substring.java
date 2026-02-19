7864802f9d74d813683ef3cc169790d27620bca1
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-19 11:15:27 +0530

    Time: 16 ms (60.37%), Space: 47.6 MB (42.12%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -17,13 +17,9 @@ class Solution {
         int startindex = -1;
 
         while(j < n) {
-            if(!map.containsKey(s.charAt(j))) {
-                map.put(s.charAt(j), -1 );
-            }
+            if(!map.containsKey(s.charAt(j))) map.put(s.charAt(j), -1 );
             else {
-                if(map.get(s.charAt(j)) > 0) {
-                    cnt += 1;
-                }
+                if(map.get(s.charAt(j)) > 0) cnt += 1;
                 map.put(s.charAt(j),  map.get(s.charAt(j)) - 1 );
             }
 
