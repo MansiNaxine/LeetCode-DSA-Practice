d518978dcd11b4ea96b6f42e0f34ec3c7ca23395
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-24 23:12:03 +0530

    Time: 1 ms (99.78%), Space: 46.8 MB (18.07%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        int carry = 0;
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        ListNode newNode = new ListNode();
+        ListNode newHead = newNode;
+        int sum = 0;
+
+        while(temp1 != null && temp2 != null) {
+            sum = temp1.val + temp2.val + carry;
+            if(newNode == null) {
+                newNode.val = sum%10;
+            } else {
+                ListNode currNode = new ListNode(sum%10);
+                newNode.next = currNode;
+                newNode = currNode;
+            }
+            carry = sum/10;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        while(temp1 != null) {
+            sum = temp1.val + carry;
+            if(newNode == null) {
+                newNode.val = sum%10;
+            } else {
+                ListNode currNode = new ListNode(sum%10);
+                newNode.next = currNode;
+                newNode = currNode;
+            }
+            carry = sum/10;
+            temp1 = temp1.next;
+        }
+
+        while(temp2 != null) {
+            sum = temp2.val + carry;
+            if(newNode == null) {
+                newNode.val = sum%10;
+            } else {
+                ListNode currNode = new ListNode(sum%10);
+                newNode.next = currNode;
+                newNode = currNode;
+            }
+            carry = sum/10;
+            temp2 = temp2.next;
+        }
+
+        if(carry > 0) {
+            ListNode currNode = new ListNode(carry);
+            newNode.next = currNode;
+        }
+
+        return newHead.next;
+    }
+}
