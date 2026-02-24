0f5d94392f079baef10a1d0216a66cf8b4611377
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-24 10:00:35 +0530

    Time: 4 ms (74.23%), Space: 94.3 MB (90.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,53 +11,47 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
-        //first get the middle element in even case we need first middle element
-        //in odd case we will get the correct one
-        ListNode middle = getMiddleElement(head);
-        ListNode nextPart = middle.next;
-        middle.next = null;
+        //Optimal Spproach
+        //1.Calculate the middle using fast and slow pointers
+        //2. Reverse the second half
+        //3.Compare the elements till half
 
-        //we have to reverse the nextPart
-        nextPart = reverseList(nextPart);
-        ListNode i = head;
-        ListNode j = nextPart;
-
-        while(i != null && j != null) {
-            if(i.val != j.val) {
-                return false;
-            }
-
-            i = i.next;
-            j = j.next;
-        }
-
-        return true;
-        
-    }
-
-    public ListNode reverseList(ListNode head) {
-        ListNode temp = head;
-        ListNode prev = null;
-
-        while(temp != null) {
-            ListNode next = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = next;
-        }
-
-        return prev;
-    }
-
-    public ListNode getMiddleElement(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null && fast.next.next != null) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        return slow;
+        ListNode nextHead = reverseList(slow);
+        ListNode temp1 = head;
+        ListNode temp2 = nextHead;
+
+        while(temp1 != null && temp2!= null) {
+            if(temp1.val != temp2.val) return false;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return true;
+
+        
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode nextNode = null;
+
+        while(temp != null) {
+            nextNode = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = nextNode;
+        }
+
+        return prev;
     }
 }
