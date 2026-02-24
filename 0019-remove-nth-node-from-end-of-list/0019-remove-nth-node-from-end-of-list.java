b271e3deb187f64e5a669ee08ed0f295f8c1189d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-24 11:13:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.16%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,31 +10,45 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
-        //first we have to calculate the count of nodes list
-        int count = 1;
-        ListNode temp = head;
-
-        while(temp.next != null) {
-            temp = temp.next;
-            count++;
+        //Edge case
+        if(head == null || head.next == null) return null;
+        ListNode temp1 = head;
+        ListNode newHead = reverseList(temp1);
+        int cnt = 0;
+        if(n == 1) {
+            newHead = newHead.next;
+            cnt = 1;
         }
-
-        int index = count - n + 1;
-        int countResult = 1;
-        temp = head;
-        ListNode prev = head;
-
-        while(countResult != index) {
-            prev = temp;
-            temp = temp.next;
-            countResult++;
-        }
-
-        prev.next = temp.next;
-
-        return count == n ? head.next : head;
-
         
+        ListNode temp = newHead;
+        
+
+        while(temp != null && temp.next != null) {
+            cnt += 1;
+            if(cnt == n - 1) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next ;
+        }
+
+        head = reverseList(newHead);
+        return head;
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode nextNode = null;
+
+        while(temp != null) {
+            nextNode = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = nextNode;
+        }
+
+        return prev;
     }
 }
