e80df76091c062f524096ffe2a95006847e672a6
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-01 16:16:44 +0530

    Time: 13 ms (26.22%), Space: 62.9 MB (15.6%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,55 +11,23 @@
 class Solution {
     public ListNode sortList(ListNode head) {
 
-        if(head==null || head.next==null){
-            return head;
+        List<Integer> arr = new ArrayList<>();
+        ListNode temp = head;
+        while(temp != null) {
+            arr.add(temp.val);
+            temp = temp.next;
         }
 
-        ListNode mid=getMid(head);
-        ListNode left=sortList(head);
-        ListNode right=sortList(mid);
+        Collections.sort(arr);
+        temp = head;
+        int index = 0;
+        while(temp != null) {
+            temp.val = arr.get(index);
+            index++;
+            temp = temp.next;
+        }
 
-
-        return merge(left,right);
+        return head;
         
-        
-    }
-
-    //funtion to merge to lists
-    public ListNode merge(ListNode left,ListNode right){
-        ListNode dummyHead=new ListNode();
-        ListNode temp=dummyHead;
-
-        while(left!=null && right!=null){
-            if(left.val<right.val){
-                temp.next=left;
-                left=left.next;
-                temp=temp.next;
-            }else{
-                temp.next=right;
-                right=right.next;
-                temp=temp.next;
-            }
-        }
-
-        temp.next=(left!=null)?left:right;
-
-        return dummyHead.next;
-    }
-
-    //function to get middle element
-    public ListNode getMid(ListNode head){
-        ListNode temp=null;
-
-        while(head!=null && head.next!=null){
-            temp=(temp==null)?head:temp.next;
-            head=head.next.next;
-            
-        }
-
-        ListNode mid=temp.next;
-            temp.next=null;
-            
-        return mid;
     }
 }
