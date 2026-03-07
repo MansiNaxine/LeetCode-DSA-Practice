4d8231b7ec9536f112677ab6f2cdd10664731658
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-08 01:01:16 +0530

    Time: 4 ms (73.43%), Space: 44.9 MB (84.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
         int[] nge = new int[m];
-        // int[] ngeNw = new int[n];
+        // int[] ngeNw = new int[n]; Modifiying original array is not a good option but still to reduce time complexity i hv done that
         Stack<Integer> st = new Stack<>();
 
         for(int i = m - 1; i >= 0; i--) {
@@ -26,7 +26,7 @@ class Solution {
         for(int i = 0; i < n ; i++ ) {
             for(int j = 0; j < m; j++) {
                 if(nums2[j] == nums1[i]) {
-                    nums1[i] = nge[j];
+                    nums1[i] = nge[j]; // break the inner loop to reduce time
                     break;
                 }
             }
