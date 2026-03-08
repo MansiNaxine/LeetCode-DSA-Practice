36476467ed3a8c847d0c9aeb5e4843703279df06
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-08 13:45:54 +0530

    Time: 30 ms (14.98%), Space: 47.7 MB (90%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] nge = new int[n];
+
+        for(int i = 0 ; i < n; i++) {
+            nge[i] = nums[i];
+        }
+
+        for(int i = 0; i < n; i++) {
+            int ele = nums[i];
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] > nums[i]) {
+                    nge[i] = nums[j];
+                    break;
+                }
+            }
+
+            if(nge[i] == ele) {
+                for(int j = 0 ; j < i; j++) {
+                    if(nums[j] > nums[i]) {
+                        nge[i] = nums[j];
+                        break;
+                    }
+                }
+            }
+
+            if(nge[i] == ele) nge[i] = -1;
+            
+        }
+
+        return nge;
+        
+    }
+}
