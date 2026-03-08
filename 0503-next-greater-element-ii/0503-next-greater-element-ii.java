3e357cc43728b7b6a63484564a22d6f4cdb9dd77
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-08 15:32:49 +0530

    Time: 17 ms (25.41%), Space: 48.3 MB (73.26%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,8 +1,34 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-        return bruteForceApproach(nums);
+        // return bruteForceApproach(nums);
+        return optimalApproach(nums);
         
+    }
+
+    public int[] optimalApproach(int[] nums) {
+
+        int n = nums.length;
+        int newN = 2 * n;
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[n];
+
+        for(int i = newN - 1; i >= 0; i--) {
+            while(!st.isEmpty() && st.peek() <= nums[i%n]) {
+                st.pop();
+            }
+
+            if(st.isEmpty()) {
+                nge[i%n] = -1;
+            }
+            else {
+                nge[i%n] = st.peek();
+            }
+
+            st.push(nums[i%n]);
+        }
+
+        return nge;
     }
 
     public int[] bruteForceApproach(int[] nums) {
