aca3ea22993ac0003b321c0e97f92b2505e5d7b4
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-10 10:39:53 +0530

    Time: 71 ms (34.18%), Space: 57.7 MB (63.9%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -35,7 +35,7 @@ class Solution {
         int[] answer = new int[n];
 
         for(int i = n - 1; i >= 0; i--) {
-            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) {
                 st.pop();
             }
 
@@ -59,7 +59,7 @@ class Solution {
         int[] answer = new int[n];
 
         for(int i = 0; i < n; i++) {
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
                 st.pop();
             }
 
