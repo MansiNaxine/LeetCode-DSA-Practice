6385c90d223b133206a9f59adf31a657a4e5acdf
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-19 17:48:26 +0530

    Time: 479 ms (16.02%), Space: 47.3 MB (48.26%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.java
+++ 0277-find-the-celebrity/0277-find-the-celebrity.java
@@ -4,8 +4,36 @@
 public class Solution extends Relation {
     public int findCelebrity(int n) {
 
-       return optimalApproach(n);
+    //    return optimalApproach(n);
+
+    return bruteForceApproach(n);
         
+    }
+
+    public int bruteForceApproach(int n) {
+        //we have to take two arrays
+        int[] iKnow = new int[n];
+        int[] knowsMe = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if((i != j) && knows(i, j)) {
+                    iKnow[i]++;
+                    knowsMe[j]++;
+                }
+            }
+        }
+
+        int ans = -1;
+
+        for(int i = 0 ; i < n; i++) {
+            if(iKnow[i] == 0 && knowsMe[i] == n - 1) {
+                ans = i;
+                break;
+            }
+        }
+
+        return ans;
     }
 
     public int optimalApproach(int n) {
