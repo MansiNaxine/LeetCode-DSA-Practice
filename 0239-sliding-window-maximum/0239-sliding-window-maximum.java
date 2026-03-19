7ca8a1ac2ea433338931b2a184c0d1e29855df9b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-19 19:42:15 +0530

    Time: 34 ms (26.81%), Space: 152.5 MB (31.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,45 +1,59 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
-        int n = nums.length;
-        Deque<Integer> q = new LinkedList<>();
-        List<Integer> arr = new ArrayList<>();
+        // return bruteForceApproach(nums, k);
 
-        //first will iterate till k size window, and will find out the maximum element
-        int i = 0; 
-        while(i < k) {
-            while(!q.isEmpty() && nums[i] > nums[q.peekLast()]) {
-                q.removeLast();
-            }
-            q.addLast(i);
-            i++;
-        }
+        return optimalApproach(nums, k);
+        
+    }
+
+    public int[] optimalApproach(int[] nums, int k) {
+
+        int n = nums.length;
+        Deque<Integer> deq = new LinkedList<>();
+        int[] ans = new int[n - k + 1];
+        int index = 0;
+        //we have to maintain window and also we have to store the max values in a decreasing oreder
+        int i = 0;
 
         while(i < n) {
-            arr.add(nums[q.peekFirst()]);
+            //first maintain the window
+            if(!deq.isEmpty() && deq.peekFirst() <= i - k) deq.removeFirst();
 
-            //loop for out of window index
-            while(!q.isEmpty() && q.peekFirst() <= i - k) {
-                q.removeFirst();
+            //store the max values in a decreasing order
+            while(!deq.isEmpty() && nums[i] >= nums[deq.peekLast()]) {
+                deq.removeLast();
             }
 
-            //checking for next Greater Element
-            while(!q.isEmpty() && nums[i] > nums[q.peekLast()]) {
-                q.removeLast();
+            //insert the latest element index in deq
+            deq.addLast(i);
+
+            //add the max element to answer
+            if(i >= k - 1) {
+                ans[index] = nums[deq.peekFirst()];
+                index++;
             }
 
-            q.addLast(i);
             i++;
         }
 
-        arr.add(nums[q.peekFirst()]);
+        return ans;
+    }
 
-        int[] answer = new int[arr.size()];
-        for(int j = 0; j < arr.size(); j++) {
-            answer[j] = arr.get(j);
+    //This will give TLE with O(N^2) time complexity
+    public int[] bruteForceApproach(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        for(int i = 0; i <= n - k; i++) {
+            int max = nums[i];
+            for(int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            ans[i] = max;
         }
 
-        return answer;
-        
+        return ans;
     }
 }
