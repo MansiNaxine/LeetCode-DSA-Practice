3ec79a9c7ce0cc5fa08d9dd8d8386687e09f57c9
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-23 23:28:28 +0530

    Time: 1 ms (24.64%), Space: 42.4 MB (60.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,32 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
 
-        List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> currSeq= new ArrayList<>();
-        helper(k, n, answer, currSeq, 1);
+        List<List<Integer>> answer =  new ArrayList<>();
+        helper(answer, k, n, new ArrayList<>(), 1, n);
         return answer;
     }
 
-    public void helper(int k , int n, List<List<Integer>> answer, List<Integer> currSeq, int index ) {
-
+    public void helper(List<List<Integer>> answer, int k, int n, List<Integer> currSeq, int index, int sum) {
         //base case
-        if(n == 0) {
-            if(currSeq.size() == k) {
+        if(index > n) {
+            if(sum == 0 && currSeq.size() == k) {
                 answer.add(new ArrayList<>(currSeq));
             }
             return;
         }
-
-        if(index > 9) return;
-
         //recursive case
-        for(int i = index; i <= 9; i++) {
-            if(n >= i) {
-                currSeq.add(i);
-                helper(k, n-i, answer, currSeq, i + 1);
-                currSeq.removeLast();
-            }
+        if(sum >= index && index <= 9) {
+            currSeq.add(index);
+            helper(answer, k, n, currSeq, index + 1, sum - index);
+            currSeq.removeLast();
         }
-
+        helper(answer, k, n, currSeq, index + 1, sum);
     }
 }
