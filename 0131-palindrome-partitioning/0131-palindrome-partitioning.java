235452b09db9a352468175b71fbf96a36fb648db
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-25 12:57:47 +0530

    Time: 9 ms (24.88%), Space: 65.4 MB (51.14%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -25,7 +25,7 @@ class Solution {
 
     public boolean isPalindrome(String s) {
         int low = 0;
-        int high = s.length()-1;
+        int high = s.length() - 1;
 
         while(low < high) {
             if(s.charAt(low) != s.charAt(high)) return false;
