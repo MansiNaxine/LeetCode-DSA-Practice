3a2cd3aca49dde4f14adaff35255ddbdc6e5aa97
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-26 12:14:19 +0530

    Time: 172 ms (25.34%), Space: 46.4 MB (16.26%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,108 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+        int[][] valid = new int[rows][cols];
+        StringBuilder sb = new StringBuilder("");
+        char ch = word.charAt(0);
+        int[] row = new int[rows];
+        int[] col = new int[cols];
+
+        //Check first letter
+        for(int i = 0 ; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(board[i][j] == ch) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        // return checkFirstLetter(word, board, rows, cols, row, 0, valid, sb);
+        for(int i = 0; i < rows; i++) {
+            if(row[i] >= 1) {
+                for(int j = 0; j < cols; j++) {
+                    if(col[j] >= 1) {
+                        valid[i][j] = 1;
+                        sb.append(board[i][j]);
+                        if( helper(valid, i, j, word, board, 1, sb, rows, cols)) return true;
+                        else {
+                            sb.deleteCharAt(sb.length()-1);
+                            valid[i][j] = 0;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        return false;
+        
+    }
+
+    // public boolean checkFirstLetter(String word, char[][] board, int rows, int cols, int row, int col, int[][] valid, StringBuilder sb) {
+    //    if(row >= rows || col >= cols) return false;
+
+    //     for(int j = col; j < cols; j++) {
+    //         if(board[row][j] == word.charAt(0)) {
+    //             valid[row][j] = 1;
+    //             col = j;
+    //             sb.append(board[row][j]);
+    //             if(helper(valid, row, col, word, board, 1, sb, rows, cols)) return true;
+    //             else {
+    //                 sb.deleteCharAt(sb.length()-1);
+    //                 valid[row][col] = 0;
+    //                 if(checkFirstLetter(word, board, rows, cols, row , col + 1, valid, sb)) return true;
+    //             }
+    //         }
+    //     }
+
+    //     return checkFirstLetter(word, board, rows, cols, row + 1, 0, valid, sb);
+    // }
+
+    public boolean helper(int[][] valid, int i, int j, String word, char[][] board, int index, StringBuilder sb, int rows, int cols) {
+       //Base case
+        if(index >= word.length()) {
+            if(sb.toString().equals(word)) return true;
+            return false;
+        }
+
+        //Recursive case
+
+        //UP direction
+
+        //DOWN direction
+        if((i+1) <= rows-1 && board[i+1][j] == word.charAt(index) && valid[i+1][j] == 0) {
+            valid[i+1][j] = 1;
+            sb.append(board[i+1][j]);
+            if(helper(valid, i+1, j, word, board, index + 1, sb, rows, cols)) return true;
+            valid[i+1][j] = 0;
+            sb.deleteCharAt(sb.length()-1);
+        }
+        //LEFT direction
+        if((j-1) >= 0 && board[i][j-1] == word.charAt(index) && valid[i][j-1] == 0) {
+            valid[i][j-1] = 1;
+            sb.append(board[i][j-1]);
+            if(helper(valid, i, j-1, word, board, index + 1, sb, rows, cols)) return true;
+            valid[i][j-1] = 0;
+            sb.deleteCharAt(sb.length()-1);
+        }
+        //RIGHT direction
+        if((j+1) <= cols-1 && board[i][j+1] == word.charAt(index) && valid[i][j+1] == 0) {
+            valid[i][j+1] = 1;
+            sb.append(board[i][j+1]);
+            if(helper(valid, i, j+1, word, board, index + 1, sb, rows, cols)) return true;
+            valid[i][j+1] = 0;
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if((i-1) >= 0 && board[i-1][j] == word.charAt(index) && valid[i-1][j] == 0) {
+            valid[i-1][j] = 1;
+            sb.append(board[i-1][j]);
+            if(helper(valid, i-1, j, word, board, index + 1, sb, rows, cols)) return true;
+            valid[i-1][j] = 0;
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        return false;
+    }
+}
