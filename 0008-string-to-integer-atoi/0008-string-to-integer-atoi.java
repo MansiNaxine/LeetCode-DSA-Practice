50e90fe57c366fcd51e3831855b6b7331064db5f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-03-28 12:34:39 +0530

    Time: 6 ms (4.28%), Space: 44.4 MB (5.52%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,75 @@
+
+import java.math.BigInteger;
+
+class Solution {
+    public int myAtoi(String s) {
+
+        StringBuilder sb = new StringBuilder("");
+        boolean neg = false;
+        boolean pos = false;
+        s = s.trim();
+        int i = 0;
+        while(i < s.length()) {
+
+
+            if (s.charAt(i) == '-' && i == 0) neg = true;
+            else if (s.charAt(i) == '+' && i == 0) pos = true;
+            else if (s.charAt(i) == 48 && sb.isEmpty()) {
+                i++;
+                continue;
+            }
+            else if(s.charAt(i) >= 48 && s.charAt(i) <= 57 ) {
+                sb.append(s.charAt(i));
+            }
+            else {
+                if(!sb.isEmpty()) {
+                    String answer = sb.toString();
+                    BigInteger answerInt = new BigInteger(answer);
+
+                    // handle negative
+                    if (neg) {
+                        answerInt = answerInt.negate();
+                    }
+
+                    // bounds check
+                    BigInteger min = BigInteger.valueOf(Integer.MIN_VALUE);
+                    BigInteger max = BigInteger.valueOf(Integer.MAX_VALUE);
+
+                    if (answerInt.compareTo(min) < 0) {
+                        return Integer.MIN_VALUE;
+                    }
+                    if (answerInt.compareTo(max) > 0) {
+                        return Integer.MAX_VALUE;
+                    }
+
+                    return answerInt.intValue();
+                }
+                else return 0;
+             }
+            i++;
+        }
+
+        String answer = sb.toString();
+        if (answer.length() == 0) return 0;
+
+        BigInteger answerInt = new BigInteger(answer);
+
+        // handle negative
+        if (neg) {
+            answerInt = answerInt.negate();
+        }
+
+        // bounds check
+        BigInteger min = BigInteger.valueOf(Integer.MIN_VALUE);
+        BigInteger max = BigInteger.valueOf(Integer.MAX_VALUE);
+
+        if (answerInt.compareTo(min) < 0) {
+            return Integer.MIN_VALUE;
+        }
+        if (answerInt.compareTo(max) > 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        return answerInt.intValue();
+            }
+}
