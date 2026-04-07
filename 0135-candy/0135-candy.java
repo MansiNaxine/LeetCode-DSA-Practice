67147ca9dd54079bc71d084cd0d9a005cb58ced9
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-04-07 12:28:17 +0530

    Time: 3 ms (86.83%), Space: 47.6 MB (59.55%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -4,20 +4,23 @@ class Solution {
        int n = ratings.length;
        int[] pre = new int[n];
        int[] suf = new int[n];
-       int cnt1 = 1;
+       int cnt = 1;
        pre[0] = 1;
-       int cnt2 = 1;
-       suf[n - 1] = cnt2;
-       
+
+       //for prefix calculation
        for(int i = 1; i < n; i++) {
-            //for prefix calculation
-            if(ratings[i] > ratings[i-1]) cnt1 += 1;
-            else cnt1 = 1;
-            pre[i] = cnt1;
-            //for suffix Calculation
-            if(ratings[n - i - 1] > ratings[(n - i - 1) + 1]) cnt2 += 1;
-            else cnt2 = 1;
-            suf[n - i - 1] = cnt2;
+            if(ratings[i] > ratings[i-1]) cnt += 1;
+            else cnt = 1;
+            pre[i] = cnt;
+        }
+
+        cnt = 1;
+        suf[n - 1] = cnt;
+        //for suffix Calculation
+        for(int i = n - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1]) cnt += 1;
+            else cnt = 1;
+            suf[i] = cnt;
         }
 
         //Calculate answer
