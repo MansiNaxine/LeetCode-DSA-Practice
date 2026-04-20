42c4d5d54d94b8d6115f14c06a56e288a8f75f95
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-04-20 11:07:06 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.17%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,16 +15,31 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        recursiveCase(root, answer);
-        return answer;
-    }
 
-    public void recursiveCase(TreeNode root, List<Integer> answer) {
-        if(root == null) return;
+        Stack<TreeNode> st = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        st.push(root);
+        while(!st.isEmpty()) {
+            TreeNode node = st.peek();
+            while(node.left != null) {
+                node = node.left;
+                st.push(node);
+            }
+            while(!st.isEmpty() && st.peek().right == null) {
+                list.add(st.peek().val);
+                st.pop();
+            }
+            if(st.isEmpty()) break;
+            TreeNode node2 = st.peek();
+            if(node2.right != null) {
+                list.add(node2.val);
+                st.pop();
+                st.push(node2.right);
+            }
+        }
+
+        return list;
         
-        recursiveCase(root.left, answer);
-        answer.add(root.val);
-        recursiveCase(root.right, answer);
     }
 }
