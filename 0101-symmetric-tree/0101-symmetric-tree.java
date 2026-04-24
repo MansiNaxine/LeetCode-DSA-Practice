1a56b1edd82bb5c3b12beebf75fac9b967450886
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-04-24 14:52:51 +0530

    Time: 0 ms (100%), Space: 43.3 MB (85.68%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,25 +14,15 @@
  * }
  */
 class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        return checkTwoTrees(root.left, root.right);
+    }
 
-    public boolean recursion(TreeNode p, TreeNode q) {
+    public boolean checkTwoTrees(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
 
-        int valP = p.val;
-        int valQ = q.val;
-
-        if(valP == valQ) {
-            boolean leftpart = recursion(p.left, q.right);
-            boolean rightpart = recursion(p.right, q.left);
-
-            return leftpart && rightpart;
-        } else {
-            return false;
-        }
-    }
-    public boolean isSymmetric(TreeNode root) {
-        
-        return recursion(root.left, root.right);
+        return (p.val == q.val) ? checkTwoTrees(p.left, q.right) && checkTwoTrees(p.right, q.left) : false;
     }
 }
