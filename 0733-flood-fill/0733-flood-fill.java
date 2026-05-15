57e8cc6eb2d5d2336d9f7e82f80d7786325a1aed
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-05-15 13:30:24 +0530

    Time: 1 ms (36.89%), Space: 46.8 MB (42%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -8,19 +8,30 @@ class Solution {
         int[][] newImage = new int[n][m];
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                newImage[i][j] = image[i][j];
+                if(image[i][j] == image[sr][sc]) {
+                    newImage[i][j] = -1;
+                }
             }
         }
 
-        int[][] visited = new int[n][m];
+        // int[][] visited = new int[n][m];
         int[] delRow = {-1, 0, +1, 0};
         int[] delCol = {0, -1, 0, +1};
 
         for(int i = 0; i < n; i++) {
             for(int j = 0 ; j < m; j++) {
-                if(i == sr && j == sc && visited[i][j] == 0) {
+                if(i == sr && j == sc) {
                     newImage[i][j] = color;
-                    dfsTraversal(visited, i, j, newImage, delRow, delCol, image[sr][sc], color);
+                    dfsTraversal( i, j, newImage, delRow, delCol, -1, color);
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n; i++) {
+            for(int j =0 ; j < m; j++) {
+                if(newImage[i][j] != color) {
+                    newImage[i][j] = image[i][j];
                 }
             }
         }
@@ -28,15 +39,15 @@ class Solution {
         return newImage;
     }
 
-    public void dfsTraversal(int[][] visited, int row, int col,int[][] newImage, int[] delRow, int[] delCol, int color, int newcolor) {
-        visited[row][col] = 1;
+    public void dfsTraversal(int row, int col,int[][] newImage, int[] delRow, int[] delCol, int color, int newcolor) {
+        // visited[row][col] = 1;
 
         for(int i = 0 ; i < 4; i++) {
             int innerRow = row + delRow[i];
             int innerCol = col + delCol[i];
-            if(innerRow >= 0 && innerRow < newImage.length && innerCol >= 0 && innerCol < newImage[0].length && visited[innerRow][innerCol] == 0 && newImage[innerRow][innerCol] == color) {
+            if(innerRow >= 0 && innerRow < newImage.length && innerCol >= 0 && innerCol < newImage[0].length && newImage[innerRow][innerCol] == color) {
                 newImage[innerRow][innerCol] = newcolor;
-                dfsTraversal(visited, innerRow, innerCol, newImage, delRow, delCol, color, newcolor);
+                dfsTraversal( innerRow, innerCol, newImage, delRow, delCol, color, newcolor);
             }
         }
     }
