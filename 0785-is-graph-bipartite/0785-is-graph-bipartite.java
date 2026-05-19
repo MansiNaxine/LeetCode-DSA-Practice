f38564f5b48e69d6317d7af7341335d13e7a30a7
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-05-19 12:44:17 +0530

    Time: 8 ms (6.42%), Space: 47.4 MB (19.9%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        
+        int n = graph.length;
+        //Create an adjancey List
+        List<List<Integer>> adjancyList = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            adjancyList.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0 ; j < graph[i].length; j++) {
+                adjancyList.get(i).add(graph[i][j]);
+            }
+        }
+
+        int[] visited = new int[n];
+        int[] visitedColour = new int[n];
+        
+
+        for(int i = 0 ; i < n; i++) {
+
+            if(visited[i] == 1) continue;
+            Queue<Integer> que = new LinkedList<>();
+            que.add(i);
+            visited[i] = 1;
+            visitedColour[i] = 0;
+
+            while(!que.isEmpty()) {
+                int root = que.remove();
+                int colour = visitedColour[root];
+
+                for(int element : adjancyList.get(root)) {
+                    if(visited[element] == 0) {
+                        visited[element] = 1;
+                        if(colour == 0) visitedColour[element] = 1;
+                        else visitedColour[element] = 0;
+                        que.add(element);
+                    }
+                    else if(visitedColour[element] == colour){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+
+    }
+}
