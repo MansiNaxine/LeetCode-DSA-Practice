e43f1c25d9a053750f8094f4c785c11bbf1a708d
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-05-26 13:53:58 +0530

    Time: 24 ms (6.6%), Space: 56.2 MB (5.2%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int countPaths(int n, int[][] roads) {
+        
+        //Creat an adjancyList;
+        List<List<List<Integer>>> adjancyList = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            adjancyList.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < roads.length; i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int d = roads[i][2];
+
+            List<Integer> currList1 = new ArrayList<>();
+            currList1.add(v);
+            currList1.add(d);
+
+            List<Integer> currList2 = new ArrayList<>();
+            currList2.add(u);
+            currList2.add(d);
+
+            adjancyList.get(u).add(new ArrayList<>(currList1));
+            adjancyList.get(v).add(new ArrayList<>(currList2));
+        }
+
+        //take Path arrays
+        //also take distance array
+        long[] distance = new long[n];
+        int[] path = new int[n];
+        Arrays.fill(distance, Long.MAX_VALUE);
+        distance[0] = 0;
+        path[0] = 1;
+        int modVal = (int)1000000007;
+
+        PriorityQueue<Pair> que = new PriorityQueue<>((x, y) -> Long.compare(x.dist , y.dist));
+        Pair p = new Pair(0, 0);
+        que.add(p);
+
+        while(!que.isEmpty()) {
+            Pair inner = que.remove();
+            long currDist = inner.dist;
+            int currNode = inner.node;
+
+            for(List<Integer> list : adjancyList.get(currNode)) {
+                long innerDist = list.get(1);
+                int innerNode = list.get(0);
+                if((currDist + innerDist) < distance[innerNode]) {
+                    path[innerNode] = path[currNode];
+                    distance[innerNode] = currDist + innerDist;
+                    Pair innerP = new Pair((currDist + innerDist), innerNode);
+                    que.add(innerP);
+                }
+                else if ((currDist + innerDist) == distance[innerNode]) {
+                    path[innerNode] = (path[innerNode] + path[currNode])%modVal;;
+                }
+            }
+        }
+
+        
+        return path[n - 1]%modVal;
+
+    }
+}
+
+class Pair {
+    long dist;
+    int node; 
+    Pair(long dist, int node) {
+        this.dist = dist;
+        this.node = node;
+    }
+}
