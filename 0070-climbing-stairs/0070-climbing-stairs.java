a9c6f8736fcd240cfc5583ed2652ff36b8b59cc8
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-05-31 15:39:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (71.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,19 +2,19 @@ class Solution {
     public int climbStairs(int n) {
 
         //TABULATION
-        int prev2 = 1;
-        int prev = 1;
-        for(int i = 2; i <= n ; i++) {
-            int eleI = prev + prev2;
-            prev2 = prev;
-            prev = eleI;
-        }
+        // int prev2 = 1;
+        // int prev = 1;
+        // for(int i = 2; i <= n ; i++) {
+        //     int eleI = prev + prev2;
+        //     prev2 = prev;
+        //     prev = eleI;
+        // }
 
-        return  prev;
+        // return  prev;
         //MEMOIZATION
-    //     int[] dp = new int[n+1];
-    //     Arrays.fill(dp, -1);
-    //     return helper(n, dp);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
      }
 
     public int helper(int n, int[] dp) {
