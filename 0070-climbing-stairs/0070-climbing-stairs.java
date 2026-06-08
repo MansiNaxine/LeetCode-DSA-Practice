3d06028d61023afa0039d0829cb57ef274269e2b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-08 10:30:59 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,14 +8,26 @@ class Solution {
         // return dpWithMemoization(ans, n);
 
         //Tabulation
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        // int[] dp = new int[n + 1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2; i <= n; i++) {
+        //     dp[i] = dp[i - 1] + dp[ i - 2];
+        // }
+
+        // return dp[n];
+
+        //In Tbaulation itself no need to take a sperate array
+        int prev1 = 1;
+        int prev = 1;
+
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[ i - 2];
+            int currI = prev + prev1;
+            prev1 = prev;
+            prev = currI;
         }
 
-        return dp[n];
+        return prev;
 
     }
 
