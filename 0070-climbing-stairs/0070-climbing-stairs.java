5d3fab92ef0167e0aeddd7365bb0a821e8db8ed8
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-08 10:29:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,9 +3,19 @@ class Solution {
         
         //Solve Using DP
         // Memoization
-        int[] ans = new int[n + 1];
-        Arrays.fill(ans, -1);
-        return dpWithMemoization(ans, n);
+        // int[] ans = new int[n + 1];
+        // Arrays.fill(ans, -1);
+        // return dpWithMemoization(ans, n);
+
+        //Tabulation
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[ i - 2];
+        }
+
+        return dp[n];
 
     }
 
