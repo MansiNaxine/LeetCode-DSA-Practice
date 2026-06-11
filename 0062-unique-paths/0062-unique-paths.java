f4d16bd166d74dff6342f1e841723528ba18f8bd
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-11 22:36:29 +0530

    Time: 1 ms (13.64%), Space: 42.2 MB (27.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -10,7 +10,32 @@ class Solution {
         //To Avoid Time Complexity go with DP
         //Memoization -- Top down approach
         int[][] dp = new int[m][n];
-        return memoRecursion(dp, row, col);
+        // return memoRecursion(dp, row, col);
+
+        //Cut down to Spcae Complexity -- avoid unnecessary Recursive space
+        return tabulationDP(dp, m, n);
+    }
+
+    public int tabulationDP(int[][] dp, int row, int col) {
+        //base cases
+        dp[0][0] = 1;
+
+        //put all the indexes from recursion into loop
+        //first loop will indicate row
+        for(int i = 0; i < row; i++) {
+            //second loop will indicate col
+            for(int j = 0; j < col; j++) {
+                if(i == 0 && j == 0) continue;
+                else {
+                    int up = (i > 0) ? dp[i - 1][j] : 0;
+                    int left = (j > 0) ? dp[i][j - 1] : 0;
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[row - 1][col - 1];
     }
 
     public int memoRecursion(int[][] dp, int row, int col) {
