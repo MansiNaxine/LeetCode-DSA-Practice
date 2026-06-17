0a68fc49f8e7f3127de3122f50392710c2903f0b
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-17 12:36:40 +0530

    Time: 179 ms (33.04%), Space: 46.3 MB (31.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -8,11 +8,33 @@ class Solution {
         }
 
         int targetNew = totalSum - target;
-        if(targetNew%2 == 1 || totalSum < target) return 0;
+        if(targetNew%2 == 1 || targetNew < 0) return 0;
         else targetNew  = targetNew/2;
+        if(totalSum < targetNew) return 0;
 
-        //bruteForce Approach
-        return recursion(nums, n - 1, targetNew);
+        // //bruteForce Approach
+        //  return recursion(nums, n - 1, targetNew);
+
+        //MEMOIZATION
+         int[][] dp = new int[n][totalSum + 1];
+         return meomRecursion(nums, n - 1, targetNew, dp);
+    }
+
+    public int meomRecursion(int[] nums, int index, int targetNew, int[][] dp) {
+        //base cases
+        if(index == 0) {
+            if(nums[index] == targetNew && targetNew == 0) return 2;
+            if(targetNew == 0 || nums[index] == targetNew ) return 1;
+            return 0;
+        }
+
+        if(dp[index][targetNew] != 0) return dp[index][targetNew];
+        //recurisve case
+        int take = 0;
+        if(nums[index] <= targetNew) take = recursion(nums, index - 1, targetNew - nums[index]);
+        int notTake = recursion(nums, index - 1, targetNew);
+
+        return dp[index][targetNew] = take + notTake;
     }
 
     public int recursion(int[] nums, int index, int targetNew) {
