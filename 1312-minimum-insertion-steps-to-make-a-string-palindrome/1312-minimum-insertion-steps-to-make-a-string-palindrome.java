afa685de3a556c6c1677267b60fa83ad4ca76bed
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-21 23:28:55 +0530

    Time: 28 ms (36.17%), Space: 47.6 MB (38.68%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int minInsertions(String s) {
+        
+         int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        String str = sb.reverse().toString();
+        int m = str.length();
+
+        //bruteFroce Approach
+        // int ans = recursion(s, str, n - 1, m - 1);
+        
+
+        //Memoization
+        int[][] dp = new int[n + 1][m + 1];
+        // int ans = meomRecursion(s, str, n, m, dp);
+
+        //tabulation
+        int ans = tabulation(s, str, n, m, dp);
+        return n - ans;
+    }
+
+    public int tabulation(String s1, String s2, int n, int m, int[][] dp) {
+        //when i = 0
+        for(int j = 0 ; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i = 0 ; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<= m; j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public int meomRecursion(String s1, String s2, int i, int j, int[][] dp) {
+        //base case
+        if(i == 0 || j == 0) return 0;
+
+        if(dp[i][j] != 0) return dp[i][j];
+        //recursive case
+        if(s1.charAt(i - 1) == s2.charAt(j - 1)) return 1 + meomRecursion(s1, s2, i - 1, j - 1, dp);
+        return dp[i][j] = Math.max(meomRecursion(s1, s2, i - 1, j, dp), meomRecursion(s1, s2, i, j - 1, dp));
+    }
+
+    public int recursion(String s1, String s2, int i, int j) {
+        //base case
+        if(i < 0 || j < 0) return 0;
+
+        //recursive case
+        if(s1.charAt(i) == s2.charAt(j)) return 1 + recursion(s1, s2, i - 1, j - 1);
+        return Math.max(recursion(s1, s2, i - 1, j), recursion(s1, s2, i, j - 1));
+    }
+}
