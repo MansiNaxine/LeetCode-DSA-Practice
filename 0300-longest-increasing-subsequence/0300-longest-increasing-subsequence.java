9aabbdc5296639a05a26acdacac33040cc919a7f
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-06-24 14:45:14 +0530

    Time: 6 ms (85.81%), Space: 46.2 MB (54.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -8,8 +8,47 @@ class Solution {
         //return recursion(nums, n, 0, -1);
 
         //MEMOIZATION
-        int[][] dp = new int[n][n];
-        return memoRecursion(nums, n, 0, -1, dp);
+        // int[][] dp = new int[n][n];
+        // return memoRecursion(nums, n, 0, -1, dp);
+
+        //USING BINARY SEARCH TECHNIQUE
+        return binarySearchSol(nums, n);
+    }
+
+    public int binarySearchSol(int[] nums, int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(nums[0]);
+        int len = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > arr.get(arr.size() - 1)) {
+                arr.add(nums[i]);
+                len++;
+            }
+            else {
+                findLowerBoundAndPlaceTheEle(arr, nums[i], n);
+            }
+        }
+
+        return len;
+    }
+
+    public void findLowerBoundAndPlaceTheEle(ArrayList<Integer> nums, int ele, int n) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int ans = 0;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+            if (nums.get(mid) >= ele) {
+                ans = mid;
+                end--;
+            }
+            else {
+                start++;
+            }
+        }
+        nums.set(ans, ele);
     }
 
     public int memoRecursion(int[] nums, int n, int index, int prev, int[][] dp) {
